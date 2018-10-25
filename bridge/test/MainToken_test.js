3f5299febee4794c3805d418bcf5c040ed999c2e
ajlopez <webmaster@ajlopez.com>
2018-10-25 10:30:42 -0300

    MainToken test refactor

--- bridge/test/MainToken_test.js
+++ bridge/test/MainToken_test.js
@@ -1,17 +1,20 @@
 const MainToken = artifacts.require('./MainToken');
 
 contract('MainToken', function (accounts) {
-    describe('creation', function () {
-        it('create MainToken', async function () {
-            this.token = await MainToken.new("MAIN", "MAIN", 18, 10000);
-            
-            const balance = await this.token.balanceOf(accounts[0]);
-            
-            assert.equal(balance, 10000);
-            
-            const totalSupply = await this.token.totalSupply();
-            
-            assert.equal(totalSupply, 10000);
-        });
+    beforeEach(async function () {
+        this.token = await MainToken.new("MAIN", "MAIN", 18, 10000);
+    });
+
+    it('initial balances', async function () {
+        this.token = await MainToken.new("MAIN", "MAIN", 18, 10000);
+        
+        const balance = await this.token.balanceOf(accounts[0]);
+        
+        assert.equal(balance, 10000);
+        
+        const totalSupply = await this.token.totalSupply();
+        
+        assert.equal(totalSupply, 10000);
     });
 });
+
