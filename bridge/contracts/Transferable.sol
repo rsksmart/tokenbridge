23bb612836954b67dd4ddb242334a44967d19961
ajlopez <webmaster@ajlopez.com>
2018-10-26 11:53:02 -0300

    acceptTransfer in interface

--- bridge/contracts/Transferable.sol
+++ bridge/contracts/Transferable.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.24;
 
 contract Transferable {
-    function acceptTransaction(address account, uint256 value) public;
+    function acceptTransfer(address account, uint256 value) public returns(bool);
 }
