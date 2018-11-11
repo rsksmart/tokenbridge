61f4e7fbf42223715ff40164ac79027823f0287e
ajlopez <webmaster@ajlopez.com>
2018-11-11 14:49:16 -0300

    Dapp loading main and side conf

--- dapps/demo/src/js/app.js
+++ dapps/demo/src/js/app.js
@@ -1,4 +1,21 @@
 
+var pages = (function () {
+    var active = null;
+    
+    function goTo(page) {
+        if (active)
+            $("#page_" + active).hide();
+        
+        $("#page_" + page).show();
+        
+        active = page;
+    }
+    
+    return {
+        goTo: goTo
+    }
+})();
+
 var app = (function () {
     var id = 0;
     var host = 'http://127.0.0.1:8545';
@@ -26,8 +43,34 @@ var app = (function () {
         post(host, request, fn);
     }
     
+    var data = {
+    };
+    
+    function loadData(fn) {
+        $.getJSON('mainconf.json', function (data1) {
+            data.accounts = data1.accounts;
+            
+            data.main = {
+                custodian: data1.custodian,
+                token: data1.token,
+                manager: data1.manager
+            };
+            
+            $.getJSON('sideconf.json', function (data2) {
+                data.main = {
+                    custodian: data1.custodian,
+                    token: data1.token,
+                    manager: data1.manager
+                };
+                
+                fn(data);
+            });
+        });
+    }
+    
     return {
-        getAccounts: getAccounts
+        getAccounts: getAccounts,
+        loadData: loadData
     }
 })();
 
