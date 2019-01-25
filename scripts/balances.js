746517a97166d8a5f171c49af3f66940a867e2d3
ajlopez <webmaster@ajlopez.com>
2019-01-25 13:52:20 -0300

    Balances script refactor

--- scripts/balances.js
+++ scripts/balances.js
@@ -1,6 +1,5 @@
 
 const rskapi = require('rskapi');
-const sasync = require('simpleasync');
 
 const chainname = process.argv[2];
 const chain = process.argv[3];
@@ -13,6 +12,7 @@ const balanceOfHash = '0x70a08231';
 console.log('chain', chainname);
 console.log('token', config.token);
 console.log('manager', config.manager);
+
 if (config.custodian)
     console.log('custodian', config.custodian);
 
@@ -21,43 +21,40 @@ console.log();
 
 var accounts;
 
-sasync()
-.exec(function (next) {    
-    var n = 0;
+var n = 0;
+
+accounts = config.accounts;
+
+if (config.token)
+    accounts.push(config.token);
+if (config.manager)
+    accounts.push(config.manager);
+if (config.custodian)
+    accounts.push(config.custodian);
+
+doGetBalance();
+
+function doGetBalance() {
+    if (n >= accounts.length) {
+        console.log();
+        return;
+    }
     
-    accounts = config.accounts;
+    const account = accounts[n++];
     
-    if (config.token)
-        accounts.push(config.token);
-    if (config.manager)
-        accounts.push(config.manager);
-    if (config.custodian)
-        accounts.push(config.custodian);
-    
-    doGetBalance();
-    
-    function doGetBalance() {
-        if (n >= accounts.length) {
-            console.log();
-            return next(null, null);
-        }
+    host.callTransaction({
+        from: accounts[0],
+        to: config.token,
+        value: '0x00',
+        data: balanceOfHash + "000000000000000000000000" + account.substring(2)
+    }, function (err, data) {
+        const balance = parseInt(data);
         
-        const account = accounts[n++];
+        console.log('balance', account, balance);
         
-        host.callTransaction({
-            from: accounts[0],
-            to: config.token,
-            value: '0x00',
-            data: balanceOfHash + "000000000000000000000000" + account.substring(2)
-        }, function (err, data) {
-            const balance = parseInt(data);
-            
-            console.log('balance', account, balance);
-            
-            doGetBalance();
-        });
-    };
-})
+        doGetBalance();
+    });
+}
 
 
 
