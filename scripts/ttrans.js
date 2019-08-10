bd70a3e1a20467c2a0190987c135f51ca8dff409
ajlopez <webmaster@ajlopez.com>
2019-08-10 17:47:32 -0300

    Improved scripts

---
+++ scripts/ttrans.js
@@ -0,0 +1,64 @@
+
+const rskapi = require('rskapi');
+const sabi = require('simpleabi');
+const txs = require('./lib/txs');
+
+const transferHash = '0xa9059cbb';
+
+const chainname = process.argv[2];
+
+const fromAccount = process.argv[3];
+const toAccount = process.argv[4];
+const amount = parseInt(process.argv[5]);
+
+const config = require('../' + chainname + 'conf.json');
+const host = rskapi.host(config.host);
+
+const balanceOfHash = '0x70a08231';
+
+console.log('chain', chainname);
+console.log('token', config.token);
+console.log('manager', config.manager);
+
+if (config.bridge)
+    console.log('bridge', config.bridge);
+
+console.log();
+
+(async function () {
+    const accounts = config.accounts;
+
+    if (config.token)
+        accounts.push(config.token);
+    if (config.manager)
+        accounts.push(config.manager);
+    if (config.bridge)
+        accounts.push(config.bridge);
+
+    var toa = toAccount[0].toLowerCase();
+    
+    var toAcc;
+    
+    if (toa === 'm')
+        toAcc = config.manager;
+    else if (toa === 'b')
+        toAcc = config.bridge;
+    else
+        toAcc = accounts[toAccount];
+
+    const abi = sabi.encodeValues([ toAcc, amount ]);
+    
+    console.log('data', abi);
+    
+    const txhash = await txs.invoke(
+        host,
+        config.token,
+        transferHash,
+        [ toAcc.address ? toAcc.address : toAcc, amount ],
+        {
+             from: accounts[fromAccount]
+        }
+    );
+    
+    console.log("tx hash", txhash);
+})();
