df4bcf3610826bb29b889bba8afc93e577ceb46a
ajlopez <webmaster@ajlopez.com>
2019-08-28 18:19:00 -0300

    ReceiptProver first test

---
+++ newcontracts/contracts/BlockRecorder.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.5.0;
+
+import "./RlpLibrary.sol";
+
+contract BlockRecorder {
+    mapping(bytes32 => bytes32) public blockData;
+    
+    function recordBlock(bytes memory blk) public {
+        bytes32 hash = keccak256(blk);   
+
+        RlpLibrary.RlpItem[] memory items = RlpLibrary.getRlpItems(blk, 0);
+        
+        bytes memory trrbytes = RlpLibrary.rlpItemToBytes(blk, items[5].offset, items[5].length);
+        bytes32 trrhash;
+        
+        assembly {
+            trrhash := mload(add(trrbytes, 0x20))
+        }
+        
+        blockData[hash] = trrhash;
+    }
+}
+
---
+++ newcontracts/contracts/ReceiptProver.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "./BlockRecorder.sol";
+import "./ProofLibrary.sol";
+
+contract ReceiptProver {
+    BlockRecorder public blockRecorder;
+    
+    constructor(BlockRecorder recorder) public {
+        blockRecorder = recorder;
+    }
+    
+    function receiptIsValid(bytes32 blkhash, bytes memory receipt, bytes[] memory prefixes, bytes[] memory suffixes) public view returns (bool) {
+        bytes32 receiptRoot = ProofLibrary.calculateRoot(receipt, prefixes, suffixes);
+        
+        return blockRecorder.blockData(blkhash) == receiptRoot;
+    }
+}
+
