ff117e58a491b765c181283b50970ad3ce8a0d31
ajlopez <webmaster@ajlopez.com>
2019-09-03 08:38:35 -0300

    EventsProcessor WIP

--- newcontracts/contracts/EventsProcessor.sol
+++ newcontracts/contracts/EventsProcessor.sol
@@ -1,21 +1,29 @@
 pragma solidity 0.5.0;
 pragma experimental ABIEncoderV2;
 
+import "./EventsLibrary.sol";
 import "./ReceiptProver.sol";
 import "./Transferable.sol";
 
 contract EventsProcessor {
     ReceiptProver public prover;
     Transferable public transferable;
+    mapping (bytes32 => bool) public processed;
     
     constructor(Transferable _transferable, ReceiptProver _prover) public {
         prover = _prover;
         transferable = _transferable;
     }
     
-    // TODO process receipt only once
     function processReceipt(bytes32 blkhash, bytes memory receipt, bytes[] memory prefixes, bytes[] memory suffixes) public {
+        bytes32 hash = keccak256(abi.encodePacked(blkhash, receipt));
+        
+        require(processed[hash] == false);
         require(prover.receiptIsValid(blkhash, receipt, prefixes, suffixes));
+        
+        
+        
+        processed[hash] = true;
     }
 }
 
