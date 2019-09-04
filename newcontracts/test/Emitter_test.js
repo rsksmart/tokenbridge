98ea6d7216a64b21c6ef8e15340d94f4a7f3af6d
ajlopez <webmaster@ajlopez.com>
2019-09-04 13:21:42 -0300

    Emitter new event Token

--- newcontracts/test/Emitter_test.js
+++ newcontracts/test/Emitter_test.js
@@ -6,11 +6,18 @@ contract('Emitter', function (accounts) {
         this.emitter = await Emitter.new();
     });
     
-    it('Emit events', async function () {
-        await this.emitter.emitEvents(
+    it('Emit cross events', async function () {
+        await this.emitter.emitCrossEvents(
             [ accounts[0], accounts[1], accounts[2] ],
             [ accounts[3], accounts[4], accounts[5] ],
             [ 1000, 2000, 3000 ]
+        );
+    });
+    
+    it('Emit token events', async function () {
+        await this.emitter.emitTokenEvents(
+            [ accounts[0], accounts[1], accounts[2] ],
+            [ "TOK1", "TOK2", "TOK3" ]
         );
     });
 });
