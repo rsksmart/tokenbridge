dca03bce33500d9ed642c30b224e7866b253205f
ajlopez <webmaster@ajlopez.com>
2019-09-04 13:46:57 -0300

    EventsProcessor with two topics

--- newcontracts/contracts/EventsProcessor.sol
+++ newcontracts/contracts/EventsProcessor.sol
@@ -9,15 +9,17 @@ contract EventsProcessor {
     ReceiptProver public prover;
     Transferable public transferable;
     address public origin;
-    bytes32 public topic;
+    bytes32 public transferTopic;
+    bytes32 public tokenTopic;
     
     mapping (bytes32 => bool) public processed;
     
-    constructor(Transferable _transferable, ReceiptProver _prover, address _origin, bytes32 _topic) public {
+    constructor(Transferable _transferable, ReceiptProver _prover, address _origin, bytes32 _transferTopic, bytes32 _tokenTopic) public {
         prover = _prover;
         transferable = _transferable;
         origin = _origin;
-        topic = _topic;
+        transferTopic = _transferTopic;
+        tokenTopic = _tokenTopic;
     }
     
     function processReceipt(bytes32 blkhash, bytes memory receipt, bytes[] memory prefixes, bytes[] memory suffixes) public {
@@ -26,7 +28,7 @@ contract EventsProcessor {
         require(processed[hash] == false);
         require(prover.receiptIsValid(blkhash, receipt, prefixes, suffixes));
         
-        EventsLibrary.TransferEvent[] memory tevents = EventsLibrary.getTransferEvents(receipt, origin, topic);
+        EventsLibrary.TransferEvent[] memory tevents = EventsLibrary.getTransferEvents(receipt, origin, transferTopic);
         uint nevents = tevents.length;
         
         for (uint k = 0; k < nevents; k++) {
