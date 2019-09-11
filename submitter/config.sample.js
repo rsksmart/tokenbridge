a3b4e4308f40fcf9e2bbe8de7dfc78273eab8aa1
Will Olivera <liamsolivera@gmail.com>
2019-09-11 16:40:54 -0300

    Created MMR sync process script

--- submitter/config.sample.js
+++ submitter/config.sample.js
@@ -1,7 +1,7 @@
 module.exports = {
     rsk: require('./regtest.json'),
     eth: require('./development.json'),
-    runEvery: 1, //In minutes
+    runEvery: 1, // In minutes,
+    mmrSyncInterval: 30, // In minutes
     confirmations: 0,
-    
 }
