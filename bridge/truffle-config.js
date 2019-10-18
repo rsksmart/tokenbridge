9db0d8f41170175090be584a1b63652360bdcd73
Will Olivera <liamsolivera@gmail.com>
2019-10-18 14:59:47 -0300

    wip: Federator voting and Bridge modifications

--- bridge/truffle-config.js
+++ bridge/truffle-config.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -20,14 +20,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 6721975
     },
     //RSK
     regtest: {
       host: "127.0.0.1",
       port: 4444,
       network_id: "*", // Match any network id
-      gas: 6800000
+      gas: 6700000
     },
     rsktestnet: {
       provider: () =>
