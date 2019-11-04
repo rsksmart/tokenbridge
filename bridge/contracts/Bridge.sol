5551583a307413a1604685b2642e9d7292144115
pedro <pedro@rsk.co>
2019-11-04 17:39:05 -0300

    receiveTokens now is payable Renamed Transferable.sol to IBridge.sol Removed Manager Contract and BasicTransferable Added Tests Added events for easier trace of events

--- bridge/contracts/Bridge.sol
+++ bridge/contracts/Bridge.sol
@@ -3,27 +3,31 @@ pragma solidity >=0.4.21 <0.6.0;
 import "./zeppelin/token/ERC20/ERC20Detailed.sol";
 import "./zeppelin/token/ERC20/SafeERC20.sol";
 import "./zeppelin/lifecycle/Pausable.sol";
+import "./zeppelin/ownership/Ownable.sol";
 import "./zeppelin/math/SafeMath.sol";
 import "./ERC677TransferReceiver.sol";
-import "./Transferable.sol";
+import "./IBridge.sol";
 import "./SideToken.sol";
 
-contract Bridge is Transferable, ERC677TransferReceiver, Pausable {
+contract Bridge is IBridge, ERC677TransferReceiver, Pausable {
     using SafeMath for uint256;
     using SafeERC20 for ERC20Detailed;
 
     address public manager;
-    uint8 symbolPrefix;
+    uint8 public symbolPrefix;
+    uint public crossingPayment;
 
     mapping (address => SideToken) public mappedTokens;
     mapping (address => address) public originalTokens;
     mapping (address => bool) public knownTokens;
-
     mapping (address => address) public mappedAddresses;
-
     mapping(bytes32 => bool) processed;
 
     event Cross(address indexed _tokenAddress, address indexed _to, uint256 _amount, string _symbol);
+    event NewSideToken(address indexed _newSideTokenAddress, address indexed _originalTokenAddress, string _symbol);
+    event AcceptedCrossTransfer(address indexed _tokenAddress, address indexed _to, uint256 _amount);
+    event CrossingPaymentChanged(uint256 _amount);
+    event ManagmentTransferred(address indexed _previousManager, address indexed _newManager);
 
     modifier onlyManager() {
         require(msg.sender == manager, "Sender is not the manager");
@@ -35,6 +39,51 @@ contract Bridge is Transferable, ERC677TransferReceiver, Pausable {
         require(_symbolPrefix != 0, "Empty symbol prefix");
         manager = _manager;
         symbolPrefix = _symbolPrefix;
+    }
+
+    function receiveTokens(ERC20Detailed tokenToUse, uint256 amount) public payable whenNotPaused returns (bool) {
+        validateToken(tokenToUse);
+        require(msg.value >= crossingPayment, "Insufficient coins sent for crossingPayment");
+        if (isSideToken(address(tokenToUse))) {
+            SideToken(address(tokenToUse)).burn(amount);
+            emit Cross(originalTokens[address(tokenToUse)], getMappedAddress(msg.sender), amount, tokenToUse.symbol());
+        }
+        else {
+            knownTokens[address(tokenToUse)] = true;
+            emit Cross(address(tokenToUse), getMappedAddress(msg.sender), amount, tokenToUse.symbol());
+        }
+        tokenToUse.safeTransferFrom(msg.sender, address(this), amount);
+        address payable receiver = address(uint160(manager));
+        receiver.transfer(msg.value);
+        return true;
+    }
+
+    function acceptTransfer(
+        address tokenAddress,
+        address receiver,
+        uint256 amount,
+        string memory symbol,
+        bytes32 blockHash,
+        bytes32 transactionHash,
+        uint32 logIndex
+    )
+        public onlyManager whenNotPaused returns(bool) {
+        require(!transactionWasProcessed(blockHash, transactionHash, receiver, amount, logIndex), "Transaction already processed");
+
+        processToken(tokenAddress, symbol);
+        processTransaction(blockHash, transactionHash, receiver, amount, logIndex);
+
+        address to = getMappedAddress(receiver);
+        if (isMappedToken(tokenAddress)) {
+            SideToken sideToken = mappedTokens[tokenAddress];
+            require(sideToken.mint(to, amount), "Error minting on side token");
+        }
+        else {
+            ERC20Detailed token = ERC20Detailed(tokenAddress);
+            token.safeTransfer(to, amount);
+        }
+        emit AcceptedCrossTransfer(tokenAddress, to, amount);
+        return true;
     }
 
     function onTokenTransfer(address to, uint256 amount, bytes memory data) public whenNotPaused returns (bool success) {
@@ -51,43 +100,22 @@ contract Bridge is Transferable, ERC677TransferReceiver, Pausable {
             string memory newSymbol = string(abi.encodePacked(symbolPrefix, symbol));
             sideToken = new SideToken(newSymbol, newSymbol);
             mappedTokens[token] = sideToken;
-            originalTokens[address(sideToken)] = token;
+            address sideTokenAddress = address(sideToken);
+            originalTokens[sideTokenAddress] = token;
+            emit NewSideToken(sideTokenAddress, token, newSymbol);
         }
     }
 
-    function acceptTransfer(
-        address tokenAddress,
-        address receiver,
-        uint256 amount,
-        string memory symbol,
-        bytes32 blockHash,
-        bytes32 transactionHash,
-        uint32 logIndex
-    )
-        public onlyManager whenNotPaused returns(bool) {
-        require(!transactionWasProcessed(blockHash, transactionHash, receiver, amount, logIndex), "Transaction already processed");
-
-        processToken(tokenAddress, symbol);
-
-        address to = getMappedAddress(receiver);
-
-        if (isMappedToken(tokenAddress)) {
-            SideToken sideToken = mappedTokens[tokenAddress];
-            require(sideToken.mint(to, amount), "Error minting on side token");
-        }
-        else {
-            ERC20Detailed token = ERC20Detailed(tokenAddress);
-            token.safeTransfer(to, amount);
-        }
-        processTransaction(blockHash, transactionHash, receiver, amount, logIndex);
-
-        return true;
+    function setCrossingPayment(uint amount) public onlyManager whenNotPaused {
+        crossingPayment = amount;
+        emit CrossingPaymentChanged(crossingPayment);
     }
 
     function changeManager(address newmanager) public onlyManager whenNotPaused {
         require(newmanager != address(0), "New manager address is empty");
-
+        address oldmanager = manager;
         manager = newmanager;
+        emit ManagmentTransferred(newmanager, oldmanager);
     }
 
     function tokenFallback(address from, uint256 amount, bytes memory) public whenNotPaused returns (bool) {
@@ -96,8 +124,8 @@ contract Bridge is Transferable, ERC677TransferReceiver, Pausable {
         address originalTokenAddress = originalTokens[msg.sender];
         require(originalTokenAddress != address(0), "Sender is not one of the crossed token contracts");
         SideToken sideToken = SideToken(msg.sender);
-        emit Cross(originalTokenAddress, getMappedAddress(from), amount, ERC20Detailed(originalTokenAddress).symbol());
         sideToken.burn(amount);
+        emit Cross(originalTokenAddress, getMappedAddress(from), amount, ERC20Detailed(originalTokenAddress).symbol());
         return true;
     }
 
@@ -117,23 +145,6 @@ contract Bridge is Transferable, ERC677TransferReceiver, Pausable {
     function validateToken(ERC20Detailed tokenToUse) private view {
         require(tokenToUse.decimals() == 18, "Token has decimals other than 18");
         require(bytes(tokenToUse.symbol()).length != 0, "Token doesn't have symbol");
-    }
-
-    function receiveTokens(ERC20Detailed tokenToUse, uint256 amount) public whenNotPaused returns (bool) {
-        //TODO should we accept  that people call receiveTokens with the SideToken???
-        validateToken(tokenToUse);
-        tokenToUse.safeTransferFrom(msg.sender, address(this), amount);
-
-        if (isSideToken(address(tokenToUse))) {
-            SideToken(address(tokenToUse)).burn(amount);
-            emit Cross(originalTokens[address(tokenToUse)], getMappedAddress(msg.sender), amount, tokenToUse.symbol());
-        }
-        else {
-            knownTokens[address(tokenToUse)] = true;
-            emit Cross(address(tokenToUse), getMappedAddress(msg.sender), amount, tokenToUse.symbol());
-        }
-
-        return true;
     }
 
     function isSideToken(address token) private view returns (bool) {
