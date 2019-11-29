0931743be7db97e6737cdf89743f42a1a2948d05
pedro <pedro@rsk.co>
2019-11-29 15:04:04 -0300

    Add ethlint to project Fix ethlint warnings an errors Add circleci config

--- bridge/contracts/SideToken.sol
+++ bridge/contracts/SideToken.sol
@@ -5,7 +5,7 @@ import "./zeppelin/token/ERC777/ERC777.sol";
 contract SideToken is ERC777 {
 
     constructor(string memory name, string memory symbol, address[] memory newDefaultOperators)
-        ERC777(name, symbol, newDefaultOperators) public {}
+    ERC777(name, symbol, newDefaultOperators) public {}
 
     function operatorMint(
         address account,
@@ -22,10 +22,10 @@ contract SideToken is ERC777 {
     function isDefaultOperator(address anAddress) public view returns(bool _isDefaultOperator) {
         _isDefaultOperator = false;
         for (uint i = 0; i < defaultOperators().length; i++) {
-                if(anAddress == defaultOperators()[i]) {
-                    _isDefaultOperator = true;
-                    break;
-                }
+            if(anAddress == defaultOperators()[i]) {
+                _isDefaultOperator = true;
+                break;
+            }
         }
         return _isDefaultOperator;
     }
