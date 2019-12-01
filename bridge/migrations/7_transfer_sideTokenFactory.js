edc15cc4c595a1aacbcb93e9891d34a9e0d443a7
pedro <pedro@rsk.co>
2019-12-01 11:02:20 -0300

    Use a migration file for each contract This is usefull to deploy in mainnet to avoid having to re deploy previous contracts if one fails

---
+++ bridge/migrations/7_transfer_sideTokenFactory.js
@@ -0,0 +1,11 @@
+const Bridge = artifacts.require("Bridge_v0");
+const SideTokenFactory = artifacts.require('SideTokenFactory');
+
+module.exports = function(deployer, networkName, accounts) {
+    deployer
+        .then(async ()=> {
+            const sideTokenFactory = await SideTokenFactory.deployed();
+            const bridge = await Bridge.deployed();
+            sideTokenFactory.transferPrimary(bridge.address);
+        });
+}
