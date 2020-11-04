3d617fd416609647dceda39a4524ec336615a993
pedro <pedro@thinkanddev.com>
2020-11-04 09:03:29 -0300

    Rename Federation to Validators

--- bridge/migrations/15_set_sideTokenFactory_v1.js
+++ bridge/migrations/15_set_sideTokenFactory_v1.js
@@ -1,7 +1,7 @@
-//We are actually gona use Bridge_v1 but truffle only knows the address of the proxy
-const Bridge = artifacts.require("Bridge_v0");
-const Bridge_v1 = artifacts.require("Bridge");
-const SideTokenFactory = artifacts.require('SideTokenFactory');
+//We are actually gona use Bridge_v2 but truffle only knows the address of the proxy
+const Bridge = artifacts.require("Bridge_v1");
+const Bridge_v2 = artifacts.require("Bridge_v2");
+const SideTokenFactory = artifacts.require('SideTokenFactory_v1');
 const MultiSigWallet = artifacts.require("MultiSigWallet");
 
 module.exports = function(deployer, networkName, accounts) {
@@ -14,8 +14,8 @@ module.exports = function(deployer, networkName, accounts) {
             const sideTokenFactory = await SideTokenFactory.deployed();
             const bridge = await Bridge.deployed();
 
-            const bridge_v1 = new web3.eth.Contract(Bridge_v1.abi, bridge.address);
-            let data = bridge_v1.methods.changeSideTokenFactory(sideTokenFactory.address).encodeABI();
+            const bridge_v2 = new web3.eth.Contract(Bridge_v2.abi, bridge.address);
+            let data = bridge_v2.methods.changeSideTokenFactory(sideTokenFactory.address).encodeABI();
 
             await multiSig.submitTransaction(bridge.address, 0, data, { from: accounts[0] });
         });
