c0ec87eaf69fc7f52e952f087751029211d522c3
pedro <pedro@thinkanddev.com>
2020-11-04 09:03:29 -0300

    Rename Federation to Validators

--- bridge/.solcover.js
+++ bridge/.solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
-    skipFiles: ['Migrations.sol','test','zeppelin', 'Bridge_v0.sol', 'Federation_v0.sol', 'SideToken_v0.sol', 'SideTokenFactory_v0.sol'],
+    skipFiles: ['Migrations.sol','test','zeppelin', 'previous'],
     providerOptions: {
       network_id: 5888,
       //vmErrorsOnRPCResponse: false
