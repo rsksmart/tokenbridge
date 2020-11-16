0d46677db5b7e53f1cae50053ab36a5771de643e
pedro <pedro@thinkanddev.com>
2020-11-16 00:43:50 -0300

    Finished rename, fixed integration test

---
+++ bridge/migrations/11_deploy_validators_v2.js
@@ -0,0 +1,18 @@
+const Validators = artifacts.require("Validators_v2");
+const Bridge = artifacts.require("Bridge_v0");
+const MultiSigWallet = artifacts.require("MultiSigWallet");
+
+module.exports = function(deployer, networkName, accounts) {
+    deployer
+        .then(async () => {
+            const validators = await deployer.deploy(Validators, [accounts[0]], 1);
+            // Replace with below line to use multiple federators
+            // return deployer.deploy(Validators, [accounts[0], accounts[1], accounts[2]], 3);
+
+            const multiSig = await MultiSigWallet.deployed();
+            const bridge = await Bridge.deployed();
+
+            await validators.setBridge(bridge.address);
+            await validators.transferOwnership(multiSig.address);
+        });
+};
