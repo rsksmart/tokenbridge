67f5cd8a1fb6ef6ad163814dbf6c346e902e9186
pedro <pedro@thinkanddev.com>
2020-11-28 01:17:31 -0300

    Dont revert if granularity is not met Instead remove the modulo of the granularity in the Bridge contract when crossing back

--- bridge/migrations/15_set_sideTokenFactory_v1.js
+++ bridge/migrations/15_set_sideTokenFactory_v1.js
@@ -1,7 +1,7 @@
 //We are actually gona use Bridge_v1 but truffle only knows the address of the proxy
 const Bridge = artifacts.require("Bridge_v0");
-const Bridge_v1 = artifacts.require("Bridge_v1");
-const SideTokenFactory = artifacts.require('SideTokenFactory_v1');
+const Bridge_v1 = artifacts.require("Bridge");
+const SideTokenFactory = artifacts.require('SideTokenFactory');
 const MultiSigWallet = artifacts.require("MultiSigWallet");
 
 module.exports = function(deployer, networkName, accounts) {
@@ -13,7 +13,7 @@ module.exports = function(deployer, networkName, accounts) {
             const multiSig = await MultiSigWallet.deployed();
             const sideTokenFactory = await SideTokenFactory.deployed();
             const bridge = await Bridge.deployed();
-            
+
             const bridge_v1 = new web3.eth.Contract(Bridge_v1.abi, bridge.address);
             let data = bridge_v1.methods.changeSideTokenFactory(sideTokenFactory.address).encodeABI();
 
