9561ce465171e5990d8ca4fc5b3abd68e6d42027
pedro <pedro@thinkanddev.com>
2021-03-18 09:53:52 -0300

    Fixing integration tests

--- bridge/migrations/20_deploy_allowTokens.js
+++ bridge/migrations/20_deploy_allowTokens.js
@@ -1,6 +1,4 @@
 //We are actually gona use the latest Bridge but truffle only knows the address of the proxy
-const BridgeProxy = artifacts.require("BridgeProxy");
-const Bridge = artifacts.require("Bridge");
 const AllowTokens = artifacts.require('AllowTokens');
 const MultiSigWallet = artifacts.require("MultiSigWallet");
 
@@ -8,12 +6,4 @@ module.exports = async (deployer, networkName, accounts) => {
     const multiSig = await MultiSigWallet.deployed();
     await deployer.deploy(AllowTokens, multiSig.address);
     const allowTokens = await AllowTokens.deployed();
-    if (networkName === 'soliditycoverage') {
-        return;
-    }
-    const bridgeProxy = await BridgeProxy.deployed();
-    const bridge = new web3.eth.Contract(Bridge.abi, bridgeProxy.address);
-    let data = bridge.methods.changeAllowTokens(allowTokens.address).encodeABI();
-
-    await multiSig.submitTransaction(bridgeProxy.address, 0, data, { from: accounts[0] });
 }
