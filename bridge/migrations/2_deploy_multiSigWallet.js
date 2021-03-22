6ca885635b5f8dcc41df82d1b571334eef89f45d
pedro <pedro@thinkanddev.com>
2021-03-22 19:39:23 -0300

    Change AllowTokens to be Upgradable

--- bridge/migrations/2_deploy_multiSigWallet.js
+++ bridge/migrations/2_deploy_multiSigWallet.js
@@ -4,8 +4,11 @@ const ProxyAdmin = artifacts.require("ProxyAdmin");
 
 module.exports = async (deployer, networkName, accounts) => {
     await deployer.deploy(MultiSigWallet, [accounts[0]], 1);
-
     // Replace with below line to use multiple federators
     // deployer.deploy(MultiSigWallet, [accounts[0], accounts[1], accounts[2]], 3);
+    const multiSig = await MultiSigWallet.deployed();
+
     await deployer.deploy(ProxyAdmin);
+    const proxyAdmin = await ProxyAdmin.deployed();
+    await proxyAdmin.transferOwnership(multiSig.address);
 };
