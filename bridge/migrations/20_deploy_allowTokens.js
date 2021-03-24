1c4d4ec210aef5dabe9c3b3d09602ca79e611ee9
pedro <pedro@thinkanddev.com>
2021-03-24 18:41:00 -0300

    AllowTokens: add Confirmations setters and tests Include in initialize the vslues of the confirmations Add more tests to allow tokens bump bridge version to 2.0.0

--- bridge/migrations/20_deploy_allowTokens.js
+++ bridge/migrations/20_deploy_allowTokens.js
@@ -13,6 +13,29 @@ module.exports = async (deployer, networkName, accounts) => {
     const multiSig = await MultiSigWallet.deployed();
     const proxyAdmin = await ProxyAdmin.deployed();
 
-    const initData = allowTokensLogic.contract.methods.initialize(multiSig.address, bridgeProxy.address).encodeABI();
+    let smallAmountConfirmations = '0';
+    let mediumAmountConfirmations = '0';
+    let largeAmountConfirmations = '0';
+    if(networkName == 'rsktestnet') {
+        smallAmountConfirmations = '2';
+        mediumAmountConfirmations = '4';
+        largeAmountConfirmations = '10';
+    }
+    if(networkName == 'kovan') {
+        smallAmountConfirmations = '5';
+        mediumAmountConfirmations = '10';
+        largeAmountConfirmations = '20';
+    }
+    if(networkName == 'rskmainnet') {
+        smallAmountConfirmations = '30';
+        mediumAmountConfirmations = '60';
+        largeAmountConfirmations = '120';
+    }
+    if(networkName == 'ethmainnet') {
+        smallAmountConfirmations = '30';
+        mediumAmountConfirmations = '60';
+        largeAmountConfirmations = '120';
+    }
+    const initData = allowTokensLogic.contract.methods.initialize(multiSig.address, bridgeProxy.address, smallAmountConfirmations, mediumAmountConfirmations , largeAmountConfirmations).encodeABI();
     await deployer.deploy(AllowTokensProxy, allowTokensLogic.address, proxyAdmin.address, initData);
 }
