887888448a7982429db4fa026d008b245c8b739c
pedro <pedro@thinkanddev.com>
2021-03-26 13:34:17 -0300

    Rename GenericBridge to BridgeFactory mainBridgeContract was only instantiated once, killing the prupose of checking for the version as it will always returns the contract created with the first version Use saveProgress only once

---
+++ federator/src/lib/BridgeFactory.js
@@ -0,0 +1,46 @@
+const abiBridgeOld = require('../../../abis/Bridge_old.json');
+const abiBridgeNew = require('../../../abis/Bridge.json');
+const BridgeInterface = require('./IBridge.js');
+const CustomError = require('./CustomError');
+
+module.exports = class BridgeFactory {
+
+    constructor(config, logger, Web3) {
+        this.config = config;
+        this.logger = logger;
+        this.mainWeb3 = new Web3(config.mainchain.host);
+        this.sideWeb3 = new Web3(config.sidechain.host);
+    }
+
+
+    async createInstance(web3, address) {
+        let bridgeContract = new web3.eth.Contract(abiBridgeOld, address);
+        const version = await bridgeContract.methods.version().call()
+        if (version === 'v3') {
+            bridgeContract = new web3.eth.Contract(abiBridgeNew, address);
+        }
+        return new BridgeInterface(bridgeContract);
+    }
+
+    async getMainBridgeContract() {
+        try {
+            return await this.createInstance(
+                this.mainWeb3,
+                this.config.mainchain.bridge
+            );
+        } catch(err) {
+            throw new CustomError(`Exception creating Main Bridge Contract`, err);
+        }
+    }
+
+    async getSideBridgeContract() {
+        try {
+            return await this.createInstance(
+                this.sideWeb3,
+                this.config.sidechain.bridge
+            );
+        } catch(err) {
+            throw new CustomError(`Exception creating Side Bridge Contract`, err);
+        }
+    }
+}
