a01a7e12196f3f9753e39ee3ed81fb6f50eefc3c
pedro <pedro@thinkanddev.com>
2021-04-08 23:10:41 -0300

    Add deployOld and upgrade npm scripts

--- bridge/migrations/11_conditional_deploy_mainToken.js
+++
@@ -1,9 +0,0 @@
-
-const MainToken = artifacts.require('MainToken');
-const deployHelper = require("../deployed/deployHelper");
-
-module.exports = async (deployer, networkName) => {
-    if(deployHelper.isLocalNetwork(networkName)) {
-        return deployer.deploy(MainToken, 'MAIN', 'MAIN', 18, web3.utils.toWei('1000'));
-    }
-}
