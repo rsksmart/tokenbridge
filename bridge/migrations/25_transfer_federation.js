fbfc911aa74dc20c635a315bd8f62989ab81fd1b
pedro <pedro@thinkanddev.com>
2021-04-08 22:15:46 -0300

    Fix linting, Upgrade Abis Use json info for transfers in migration

--- bridge/migrations/25_transfer_federation.js
+++ bridge/migrations/25_transfer_federation.js
@@ -1,15 +1,15 @@
-const BridgeProxy = artifacts.require("BridgeProxy");
 const Bridge = artifacts.require("Bridge");
-const Federation = artifacts.require('Federation');
 const MultiSigWallet = artifacts.require("MultiSigWallet");
+const deployHelper = require("../deployed/deployHelper");
 
 module.exports = async (deployer, networkName, accounts) => {
-    const multiSig = await MultiSigWallet.deployed();
-    const federation = await Federation.deployed();
-    const bridgeProxy = await BridgeProxy.deployed();
+    const deployedJson = deployHelper.getDeployed(networkName);
+    const multiSig = await MultiSigWallet.at(deployedJson.MultiSig);
 
-    const bridge = new web3.eth.Contract(Bridge.abi, bridgeProxy.address);
-    let data = bridge.methods.changeFederation(federation.address).encodeABI();
-    await multiSig.submitTransaction(bridgeProxy.address, 0, data, { from: accounts[0] });
+    const bridge = new web3.eth.Contract(Bridge.abi, deployedJson.BridgeProxy);
+    const data = bridge.methods.changeFederation(deployedJson.Federation).encodeABI();
+
+    await bridge.methods.changeFederation(deployedJson.Federation).call({ from: deployedJson.MultiSig });
+    await multiSig.submitTransaction(deployedJson.BridgeProxy, 0, data, { from: accounts[0] });
 
 }
