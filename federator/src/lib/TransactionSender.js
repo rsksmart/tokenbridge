7e10039f04d4e9686ff24fc518b13382e69b84f8
pedro <pedro@thinkanddev.com>
2021-04-16 11:32:22 -0300

    Bump gas price, add rawtx to log, reduce gas price multiplier

--- federator/src/lib/TransactionSender.js
+++ federator/src/lib/TransactionSender.js
@@ -26,7 +26,6 @@ module.exports = class TransactionSender {
 
     async getGasPrice(chainId) {
         chainId = parseInt(chainId)
-        console.log(chainId)
         if(chainId>= 30 && chainId <=33) {
             return this.getRskGasPrice();
         }
@@ -35,13 +34,13 @@ module.exports = class TransactionSender {
 
     async getGasLimit(rawTx) {
         let estimatedGas = await this.client.eth.estimateGas({ gasPrice: rawTx.gasPrice, value: rawTx.value, to: rawTx.to, data: rawTx.data, from: rawTx.from});
-        estimatedGas = (estimatedGas < 250000) ? 250000 : 3500000;
+        estimatedGas = (estimatedGas < 300_000) ? 300_000 : 3_500_000;
         return estimatedGas;
     }
 
     async getEthGasPrice() {
         const gasPrice = parseInt(await this.client.eth.getGasPrice());
-        return gasPrice <= 1 ? 1: Math.round(gasPrice * 1.5);
+        return gasPrice <= 1 ? 1: Math.round(gasPrice * 1.3);
     }
 
     async getRskGasPrice() {
@@ -66,6 +65,7 @@ module.exports = class TransactionSender {
             s: 0
         }
         rawTx.gas = this.numberToHexString(await this.getGasLimit(rawTx));
+        this.logger.debug('RawTx', rawTx);
         return rawTx;
     }
 
