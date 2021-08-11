059c9aa7793ed7fd46f0d77cfad3d4cfbade4257
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-10 21:21:53 -0300

    fix lint for Proxies

--- bridge/contracts/Proxies.sol
+++ bridge/contracts/Proxies.sol
@@ -6,13 +6,16 @@ pragma abicoder v2;
 import "./zeppelin/upgradable/proxy/TransparentUpgradeableProxy.sol";
 
 contract BridgeProxy is TransparentUpgradeableProxy {
-    constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic,_admin, _data) payable {}
+  // solhint-disable-next-line no-empty-blocks
+  constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic,_admin, _data) payable {}
 }
 
 contract AllowTokensProxy is TransparentUpgradeableProxy {
-    constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic,_admin, _data) payable {}
+  // solhint-disable-next-line no-empty-blocks
+  constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic,_admin, _data) payable {}
 }
 
 contract FederationProxy is TransparentUpgradeableProxy {
-    constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic,_admin, _data) payable {}
+  // solhint-disable-next-line no-empty-blocks
+  constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic,_admin, _data) payable {}
 }
