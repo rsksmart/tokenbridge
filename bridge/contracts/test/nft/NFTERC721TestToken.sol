b39e94dab7cfafd633e50e83049ca4e0ffa2f2d4
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-11 13:39:15 -0300

    update the harness token MainTokenNFTerc721 to NFTERC721TestToken

--- bridge/contracts/test/nft/NFTERC721TestToken.sol
+++ bridge/contracts/test/nft/NFTERC721TestToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-import "../zeppelin/token/ERC721/ERC721.sol";
+import "../../zeppelin/token/ERC721/ERC721.sol";
 
-contract MainTokenNFTerc721 is ERC721 {
+contract NFTERC721TestToken is ERC721 {
 
   constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
 
