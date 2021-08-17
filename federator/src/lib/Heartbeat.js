08f2ecb1c21b65e74c8f3b82ede538f5e797549b
pedro <pedro@thinkanddev.com>
2021-08-17 14:00:44 -0300

    Fix Heartbeat data error

--- federator/src/lib/Heartbeat.js
+++ federator/src/lib/Heartbeat.js
@@ -209,7 +209,7 @@ module.exports = class Heartbeat {
 
     _saveProgress (path, value) {
         if (value) {
-            fs.writeFileSync(path, value);
+            fs.writeFileSync(path, value.toString());
         }
     }
 
