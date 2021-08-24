b8ed33b2a0830dabb6f485d08e25bd99c2fb9ee5
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-24 11:59:00 -0300

    refactory to token bridge nft federator read

--- federator/src/lib/FederatorNFT.ts
+++ federator/src/lib/FederatorNFT.ts
@@ -8,9 +8,9 @@ import TransactionSender from './TransactionSender';
 import CustomError from './CustomError';
 import BridgeFactory from '../contracts/BridgeFactory';
 import FederationFactory from '../contracts/FederationFactory';
-import AllowTokensFactory from '../contracts/AllowTokensFactory';
 import utils from './utils';
 
+const NFT_CONFIRMATION_BLOCKS = 5;
 export class FederatorNFT {
   public logger: Logger;
   public config: Config;
@@ -22,11 +22,13 @@ export class FederatorNFT {
   public revertedTxnsPath: string;
   public bridgeFactory: BridgeFactory;
   public federationFactory: FederationFactory;
-  public allowTokensFactory: AllowTokensFactory;
+  public nftConfirmationsBlocks: number;
+  private federatorContract: import('../contracts/IFederationV2');
 
-  constructor(config: Config, logger: Logger, Web3 = web3) {
+  constructor(config: Config, logger: Logger, Web3 = web3, nftConfirmationsBlocks: number = NFT_CONFIRMATION_BLOCKS) {
     this.config = config;
     this.logger = logger;
+    this.nftConfirmationsBlocks = nftConfirmationsBlocks;
 
     if (!utils.checkHttpsOrLocalhost(config.mainchain.host)) {
       throw new Error(`Invalid host configuration, https or localhost required`);
@@ -40,7 +42,13 @@ export class FederatorNFT {
     this.revertedTxnsPath = `${config.storagePath || __dirname}/revertedTxns.json`;
     this.bridgeFactory = new BridgeFactory(this.config, this.logger, Web3);
     this.federationFactory = new FederationFactory(this.config, this.logger, Web3);
-    this.allowTokensFactory = new AllowTokensFactory(this.config, this.logger, Web3);
+  }
+
+  async getFederator(): Promise<import('../contracts/IFederationV2')> {
+    if (this.federatorContract == null) {
+      this.federatorContract = await this.federationFactory.getSideFederationContract();
+    }
+    return this.federatorContract;
   }
 
   async run(): Promise<boolean> {
@@ -73,31 +81,20 @@ export class FederatorNFT {
         }
 
         this.logger.debug(`Current Block ${currentBlock} ChainId ${chainId}`);
-        const allowTokens = await this.allowTokensFactory.getMainAllowTokensContract();
-        const confirmations = await allowTokens.getConfirmations();
-        const toBlock = currentBlock - confirmations.largeAmountConfirmations;
-        const newToBlock = currentBlock - confirmations.smallAmountConfirmations;
+        const toBlock = currentBlock - this.nftConfirmationsBlocks;
 
         this.logger.info('Running to Block', toBlock);
 
-        if (toBlock <= 0 && newToBlock <= 0) {
+        if (toBlock <= 0) {
           return false;
         }
 
         if (!fs.existsSync(this.config.storagePath)) {
           fs.mkdirSync(this.config.storagePath);
         }
-        const originalFromBlock = this.config.mainchain.fromBlock || 0;
-        let fromBlock = null;
-        try {
-          fromBlock = parseInt(fs.readFileSync(this.lastBlockPath, 'utf8'));
-        } catch (err) {
-          fromBlock = originalFromBlock;
-        }
-        if (fromBlock < originalFromBlock) {
-          fromBlock = originalFromBlock;
-        }
-        if (fromBlock >= toBlock && fromBlock >= newToBlock) {
+
+        let fromBlock = this.getLastBlock();
+        if (fromBlock >= toBlock) {
           this.logger.warn(
             `Current chain ${chainId} Height ${toBlock} is the same or lesser than the last block processed ${fromBlock}`,
           );
@@ -105,10 +102,7 @@ export class FederatorNFT {
         }
         fromBlock = fromBlock + 1;
         this.logger.debug('Running from Block', fromBlock);
-        await this.getLogsAndProcess(fromBlock, toBlock, currentBlock, false, confirmations);
-        const lastBlockProcessed = toBlock;
-        await this.getLogsAndProcess(lastBlockProcessed, newToBlock, currentBlock, true, confirmations);
-
+        await this.getLogsAndProcess(fromBlock, toBlock, currentBlock);
         return true;
       } catch (err) {
         this.logger.error(new Error('Exception Running Federator'), err);
@@ -123,7 +117,21 @@ export class FederatorNFT {
     }
   }
 
-  async getLogsAndProcess(fromBlock: number, toBlock: number, currentBlock: number, medmiumAndSmall: boolean, confirmations: { smallAmountConfirmations?: any; mediumAmountConfirmations: any; largeAmountConfirmations: any; }) {
+  getLastBlock(): number {
+    let fromBlock: number = null;
+    const originalFromBlock = this.config.mainchain.fromBlock || 0;
+    try {
+      fromBlock = parseInt(fs.readFileSync(this.lastBlockPath, 'utf8'));
+    } catch (err) {
+      fromBlock = originalFromBlock;
+    }
+    if (fromBlock < originalFromBlock) {
+      fromBlock = originalFromBlock;
+    }
+    return fromBlock;
+  }
+
+  async getLogsAndProcess(fromBlock: number, toBlock: number, currentBlock: number): Promise<void> {
     if (fromBlock >= toBlock) return;
 
     const mainBridge = await this.bridgeFactory.getMainBridgeContract();
@@ -146,140 +154,86 @@ export class FederatorNFT {
       if (!logs) throw new Error('Failed to obtain the logs');
 
       this.logger.info(`Found ${logs.length} logs`);
-      await this._processLogs(logs, currentBlock, medmiumAndSmall, confirmations);
-      if (!medmiumAndSmall) {
-        this._saveProgress(this.lastBlockPath, toPagedBlock.toString());
-      }
+      await this._processLogs(logs, currentBlock);
+      this._saveProgress(this.lastBlockPath, toPagedBlock.toString());
       fromPageBlock = toPagedBlock + 1;
     }
   }
 
-  async _processLogs(
-    logs: any,
-    currentBlock: number,
-    mediumAndSmall: any,
-    confirmations: { mediumAmountConfirmations: any; largeAmountConfirmations: any },
-  ): Promise<boolean> {
+  async _processLogs(logs: any, currentBlock: number): Promise<boolean> {
     try {
       const from = await this.transactionSender.getAddress(this.config.privateKey);
-      const fedContract = await this.federationFactory.getSideFederationContract();
-      const allowTokens = await this.allowTokensFactory.getMainAllowTokensContract();
+      const fedContract = await this.getFederator();
 
       const isMember = await utils.retry3Times(fedContract.isMember(from).call);
       if (!isMember) throw new Error(`This Federator addr:${from} is not part of the federation`);
-
-      const { mediumAmountConfirmations, largeAmountConfirmations } = confirmations;
 
       for (const log of logs) {
         this.logger.info('Processing event log:', log);
 
         const { blockHash, transactionHash, logIndex, blockNumber } = log;
 
-        const {
-          _to: receiver,
-          _from: crossFrom,
-          _amount: amount,
-          _symbol: symbol,
-          _tokenAddress: tokenAddress,
-          _decimals: decimals,
-          _granularity: granularity,
-          _typeId: typeId,
-        } = log.returnValues;
+        const { _to: receiver, _from: sender, _amount: amount, _tokenAddress: originalTokenAddress } = log.returnValues;
 
         const mainBridge = await this.bridgeFactory.getMainBridgeContract();
-        const sideTokenAddress = await utils.retry3Times(mainBridge.getMappedToken(tokenAddress).call);
-        let allowed: any, mediumAmount: string | number, largeAmount: string | number;
+        const sideTokenAddress = await utils.retry3Times(mainBridge.getMappedToken(originalTokenAddress).call);
         if (sideTokenAddress == utils.zeroAddress) {
-          ({ allowed, mediumAmount, largeAmount } = await allowTokens.getLimits(tokenAddress));
-          if (!allowed) {
-            throw new Error(
-              `Original Token not allowed nor side token Tx:${transactionHash} originalTokenAddress:${tokenAddress}`,
-            );
-          }
-        } else {
-          ({ allowed, mediumAmount, largeAmount } = await allowTokens.getLimits(sideTokenAddress));
-          if (!allowed) {
-            this.logger.error(
-              `Side token:${sideTokenAddress} needs to be allowed Tx:${transactionHash} originalTokenAddress:${tokenAddress}`,
-            );
-          }
+          throw new Error(
+            `Side token is zero Address Tx:${transactionHash} originalTokenAddress:${originalTokenAddress}`,
+          );
         }
 
-        const mediumAmountBN = this.mainWeb3.utils.toBN(mediumAmount);
-        const largeAmountBN = this.mainWeb3.utils.toBN(largeAmount);
-        const amountBN = this.mainWeb3.utils.toBN(amount);
-
-        if (mediumAndSmall) {
-          // At this point we're processing blocks newer than largeAmountConfirmations
-          // and older than smallAmountConfirmations
-          if (amountBN.gte(largeAmountBN)) {
-            const c = currentBlock - blockNumber;
-            const rC = largeAmountConfirmations;
-            this.logger.debug(
-              `[large amount] Tx: ${transactionHash} ${amount} originalTokenAddress:${tokenAddress} won't be proccessed yet ${c} < ${rC}`,
-            );
-            continue;
-          }
-
-          if (amountBN.gte(mediumAmountBN) && currentBlock - blockNumber < mediumAmountConfirmations) {
-            const c = currentBlock - blockNumber;
-            const rC = mediumAmountConfirmations;
-            this.logger.debug(
-              `[medium amount] Tx: ${transactionHash} ${amount} originalTokenAddress:${tokenAddress} won't be proccessed yet ${c} < ${rC}`,
-            );
-            continue;
-          }
+        const blocksConfirmed = currentBlock - blockNumber;
+        if (blocksConfirmed < this.nftConfirmationsBlocks) {
+          this.logger.debug(
+            `[NFT] Tx: originalTokenAddress:${originalTokenAddress} won't be proccessed yet ${blocksConfirmed} < ${this.nftConfirmationsBlocks}`,
+          );
+          continue;
         }
 
         const transactionId = await utils.retry3Times(
           fedContract.getTransactionId({
-            originalTokenAddress: tokenAddress,
-            sender: crossFrom,
+            originalTokenAddress,
+            sender,
             receiver,
             amount,
-            symbol,
             blockHash,
             transactionHash,
             logIndex,
-            decimals,
-            granularity,
-            typeId,
           }).call,
         );
         this.logger.info('get transaction id:', transactionId);
 
         const wasProcessed = await utils.retry3Times(fedContract.transactionWasProcessed(transactionId).call);
-        if (!wasProcessed) {
-          const hasVoted = await fedContract.hasVoted(transactionId).call({ from });
-          if (!hasVoted) {
-            this.logger.info(
-              `Voting tx: ${log.transactionHash} block: ${log.blockHash} originalTokenAddress: ${tokenAddress}`,
-            );
-            await this._voteTransaction(
-              fedContract,
-              tokenAddress,
-              crossFrom,
-              receiver,
-              amount,
-              symbol,
-              log.blockHash,
-              log.transactionHash,
-              log.logIndex,
-              decimals,
-              granularity,
-              typeId,
-              transactionId,
-            );
-          } else {
-            this.logger.debug(
-              `Block: ${log.blockHash} Tx: ${log.transactionHash} originalTokenAddress: ${tokenAddress}  has already been voted by us`,
-            );
-          }
-        } else {
+        if (wasProcessed) {
           this.logger.debug(
-            `Block: ${log.blockHash} Tx: ${log.transactionHash} originalTokenAddress: ${tokenAddress} was already processed`,
+            `Block: ${log.blockHash} Tx: ${log.transactionHash} originalTokenAddress: ${originalTokenAddress} was already processed`,
           );
+          continue;
         }
+
+        const hasVoted = await fedContract.hasVoted(transactionId).call({ from });
+        if (!hasVoted) {
+          this.logger.debug(
+            `Block: ${log.blockHash} Tx: ${log.transactionHash} originalTokenAddress: ${originalTokenAddress}  has already been voted by us`,
+          );
+          continue;
+        }
+
+        this.logger.info(
+          `Voting tx: ${log.transactionHash} block: ${log.blockHash} originalTokenAddress: ${originalTokenAddress}`,
+        );
+
+        await this._voteTransaction(
+          originalTokenAddress,
+          sender,
+          receiver,
+          amount,
+          log.blockHash,
+          log.transactionHash,
+          log.logIndex,
+          transactionId,
+        );
       }
 
       return true;
@@ -289,95 +243,112 @@ export class FederatorNFT {
   }
 
   async _voteTransaction(
-    fedContract: import('../contracts/IFederationV2') | import('../contracts/IFederationV1'),
-    tokenAddress: any,
-    sender: any,
-    receiver: any,
-    amount: any,
-    symbol: any,
-    blockHash: any,
-    transactionHash: any,
-    logIndex: any,
-    decimals: any,
-    granularity: any,
-    typeId: any,
+    originalTokenAddress: string,
+    sender: string,
+    receiver: string,
+    tokenId: number,
+    blockHash: string,
+    transactionHash: string,
+    logIndex: number,
     txId: string,
   ): Promise<boolean> {
     try {
       txId = txId.toLowerCase();
       this.logger.info(
-        `Voting Transfer ${amount} of originalTokenAddress:${tokenAddress} trough sidechain bridge ${this.config.sidechain.bridge} to receiver ${receiver}`,
+        `Voting Transfer ${tokenId} of originalTokenAddress:${originalTokenAddress} trough sidechain bridge ${this.config.sidechain.bridge} to receiver ${receiver}`,
       );
 
-      const txData = await fedContract
+      const fedContract = await this.getFederator();
+      const voteTransactionTxData = await fedContract
         .voteTransaction({
-          originalTokenAddress: tokenAddress,
+          originalTokenAddress,
           sender,
           receiver,
-          amount,
-          symbol,
+          amount: tokenId,
           blockHash,
           transactionHash,
           logIndex,
-          decimals,
-          granularity,
-          typeId,
-          tokenType: utils.tokenType.COIN,
+          tokenType: utils.tokenType.NFT,
         })
         .encodeABI();
 
-      let revertedTxns = {};
-      if (fs.existsSync(this.revertedTxnsPath)) {
-        revertedTxns = JSON.parse(fs.readFileSync(this.revertedTxnsPath, 'utf8'));
-        this.logger.info(`read these transactions from reverted transactions file`, revertedTxns);
-      }
-
-      if (revertedTxns[txId]) {
-        this.logger.info(
-          `Skipping Voting ${amount} of originalTokenAddress:${tokenAddress} TransactionId ${txId} since it's marked as reverted.`,
-          revertedTxns[txId],
-        );
-        return false;
-      }
-
-      const receipt = await this.transactionSender.sendTransaction(
+      return await this.handleRevertedTxns(
+        originalTokenAddress,
+        sender,
+        receiver,
+        tokenId,
+        blockHash,
+        transactionHash,
+        logIndex,
+        txId,
         fedContract.getAddress(),
-        txData,
-        0,
-        this.config.privateKey,
+        voteTransactionTxData,
       );
-
-      if (receipt.status == false) {
-        fs.writeFileSync(
-          this.revertedTxnsPath,
-          JSON.stringify({
-            ...revertedTxns,
-            [txId]: {
-              originalTokenAddress: tokenAddress,
-              sender,
-              receiver,
-              amount,
-              symbol,
-              blockHash,
-              transactionHash,
-              logIndex,
-              decimals,
-              granularity,
-            },
-          }),
-        );
-      }
-
-      return true;
     } catch (err) {
       throw new CustomError(
-        `Exception Voting tx:${transactionHash} block: ${blockHash} originalTokenAddress: ${tokenAddress}`,
+        `Exception Voting tx:${transactionHash} block: ${blockHash} originalTokenAddress: ${originalTokenAddress}`,
         err,
       );
     }
   }
 
-  _saveProgress(path: fs.PathOrFileDescriptor, value: string) {
+  async handleRevertedTxns(
+    originalTokenAddress: string,
+    sender: string,
+    receiver: string,
+    tokenId: number,
+    blockHash: string,
+    transactionHash: string,
+    logIndex: number,
+    txId: string,
+    federationContractAddress: string,
+    voteTransactionTxData: any,
+  ): Promise<boolean> {
+    let revertedTxns = {};
+    if (fs.existsSync(this.revertedTxnsPath)) {
+      revertedTxns = JSON.parse(fs.readFileSync(this.revertedTxnsPath, 'utf8'));
+      this.logger.info(`read these transactions from reverted transactions file`, revertedTxns);
+    }
+
+    if (revertedTxns[txId]) {
+      this.logger.info(
+        `Skipping Voting ${tokenId} of originalTokenAddress:${originalTokenAddress} TransactionId ${txId} since it's marked as reverted.`,
+        revertedTxns[txId],
+      );
+      return false;
+    }
+
+    const receipt = await this.transactionSender.sendTransaction(
+      federationContractAddress,
+      voteTransactionTxData,
+      0,
+      this.config.privateKey,
+    );
+
+    if (receipt.status == false) {
+      this.writeRevertedTxns(
+        JSON.stringify({
+          ...revertedTxns,
+          [txId]: {
+            originalTokenAddress,
+            sender,
+            receiver,
+            tokenId,
+            blockHash,
+            transactionHash,
+            logIndex,
+          },
+        }),
+      );
+    }
+    return receipt.status;
+  }
+
+  writeRevertedTxns(content: string): void {
+    fs.writeFileSync(this.revertedTxnsPath, content);
+  }
+
+  _saveProgress(path: fs.PathOrFileDescriptor, value: string): void {
     if (value) {
       fs.writeFileSync(path, value.toString());
     }
