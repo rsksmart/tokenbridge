2f9ed97f6b6b01f736f6879dee39bd1e529518dd
Marccio Silva <marccio@iovlabs.org>
2021-08-26 14:51:56 -0300

    Add proxies as named accounts, check if they exist before deploying them in migrations, refactor associated files (having added Prettier config for JS files)

--- bridge/deploy/02_deploy_proxyAdmin.js
+++ bridge/deploy/02_deploy_proxyAdmin.js
@@ -1,9 +1,9 @@
-module.exports = async function({getNamedAccounts, deployments}) { // HardhatRuntimeEnvironment
-  const {deployer, multiSig, proxyAdmin} = await getNamedAccounts();
-  const {deploy, log, execute} = deployments;
+module.exports = async function({ getNamedAccounts, deployments }) { // HardhatRuntimeEnvironment
+  const { deployer, multiSig, proxyAdmin } = await getNamedAccounts();
+  const { deploy, log, execute } = deployments;
 
   if (!proxyAdmin) {
-    const deployResult = await deploy('ProxyAdmin', {
+    const deployResult = await deploy("ProxyAdmin", {
       from: deployer,
       log: true
     });
@@ -12,12 +12,12 @@ module.exports = async function({getNamedAccounts, deployments}) { // HardhatRun
       log(
         `Contract ProxyAdmin deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed.toString()} gas`
       );
-      const MultiSigWallet = await deployments.get('MultiSigWallet');
-      await execute('ProxyAdmin', {from: deployer}, 'transferOwnership', multiSig ?? MultiSigWallet.address);
+      const MultiSigWallet = await deployments.get("MultiSigWallet");
+      await execute("ProxyAdmin", { from: deployer }, "transferOwnership", multiSig ?? MultiSigWallet.address);
       log(`Transfered Ownership to MultiSig`);
     }
   }
 };
-module.exports.id = 'deploy_proxyAdmin'; // id required to prevent reexecution
-module.exports.tags = ['ProxyAdmin'];
-module.exports.dependencies = ['MultiSigWallet'];
+module.exports.id = "deploy_proxyAdmin"; // id required to prevent reexecution
+module.exports.tags = ["ProxyAdmin"];
+module.exports.dependencies = ["MultiSigWallet"];
