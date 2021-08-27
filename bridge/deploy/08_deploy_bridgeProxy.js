7ed782646394c2754a2fcd9baade63aae9c46511
Marccio Silva <marccio@iovlabs.org>
2021-08-27 10:02:24 -0300

    Add missing prettier dependency, move prettier config to package.json for bridge JS files, reformat all files related to PR, use bridge proxy in 22_deploy_federation if available instead of deploying the contract

--- bridge/deploy/08_deploy_bridgeProxy.js
+++ bridge/deploy/08_deploy_bridgeProxy.js
@@ -1,20 +1,20 @@
-module.exports = async function({ getNamedAccounts, deployments, network }) { // HardhatRuntimeEnvironment
-  const { deployer, multiSig, proxyAdmin, bridgeProxy } = await getNamedAccounts();
-  const { deploy, log } = deployments;
+module.exports = async function({getNamedAccounts, deployments, network}) { // HardhatRuntimeEnvironment
+  const {deployer, multiSig, proxyAdmin, bridgeProxy} = await getNamedAccounts();
+  const {deploy, log} = deployments;
 
   if (!bridgeProxy) {
-    let symbol = "e";
-    if (network.name === "rskregtest" || network.name === "rsktestnet" || network.name === "rskmainnet") {
-      symbol = "r";
+    let symbol = 'e';
+    if (network.name === 'rskregtest' || network.name === 'rsktestnet' || network.name === 'rskmainnet') {
+      symbol = 'r';
     }
 
-    const MultiSigWallet = await deployments.get("MultiSigWallet");
-    const ProxyAdmin = await deployments.get("ProxyAdmin");
-    const Federation_old = await deployments.get("Federation_old");
-    const AllowTokens_old = await deployments.get("AllowTokens_old");
-    const SideTokenFactory_old = await deployments.get("SideTokenFactory_old");
+    const MultiSigWallet = await deployments.get('MultiSigWallet');
+    const ProxyAdmin = await deployments.get('ProxyAdmin');
+    const Federation_old = await deployments.get('Federation_old');
+    const AllowTokens_old = await deployments.get('AllowTokens_old');
+    const SideTokenFactory_old = await deployments.get('SideTokenFactory_old');
 
-    const Bridge_old = await deployments.get("Bridge_old");
+    const Bridge_old = await deployments.get('Bridge_old');
     const bridge = new web3.eth.Contract(Bridge_old.abi, Bridge_old.address);
     const methodCall = bridge.methods.initialize(
       multiSig ?? MultiSigWallet.address,
@@ -23,9 +23,9 @@ module.exports = async function({ getNamedAccounts, deployments, network }) { //
       SideTokenFactory_old.address,
       symbol
     );
-    await methodCall.call({ from: deployer });
+    await methodCall.call({from: deployer});
 
-    const deployProxyResult = await deploy("BridgeProxy", {
+    const deployProxyResult = await deploy('BridgeProxy', {
       from: deployer,
       args: [Bridge_old.address, proxyAdmin ?? ProxyAdmin.address, methodCall.encodeABI()],
       log: true
@@ -35,6 +35,6 @@ module.exports = async function({ getNamedAccounts, deployments, network }) { //
     }
   }
 };
-module.exports.id = "deploy_bridgeProxy"; // id required to prevent reexecution
-module.exports.tags = ["BridgeProxy", "old"];
-module.exports.dependencies = ["Bridge_old", "MultiSigWallet", "ProxyAdmin", "Federation_old", "AllowTokens_old", "SideTokenFactory_old"];
+module.exports.id = 'deploy_bridgeProxy'; // id required to prevent reexecution
+module.exports.tags = ['BridgeProxy', 'old'];
+module.exports.dependencies = ['Bridge_old', 'MultiSigWallet', 'ProxyAdmin', 'Federation_old', 'AllowTokens_old', 'SideTokenFactory_old'];
