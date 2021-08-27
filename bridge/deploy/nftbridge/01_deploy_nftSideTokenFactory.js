5741d03c663024ff6a8f3b9d7e8619d8c659a1fc
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-27 11:49:49 -0300

    Add deployment for Federation with NFT

---
+++ bridge/deploy/nftbridge/01_deploy_nftSideTokenFactory.js
@@ -0,0 +1,15 @@
+module.exports = async function ({getNamedAccounts, deployments}) { // HardhatRuntimeEnvironment
+  const {deployer} = await getNamedAccounts()
+  const {deploy, log} = deployments
+
+  const deployResult = await deploy('SideNFTTokenFactory', {
+    from: deployer,
+    log: true,
+  });
+
+  if (deployResult.newlyDeployed) {
+    log(`Contract SideNFTTokenFactory deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed.toString()} gas`);
+  }
+};
+module.exports.id = 'deploy_nft_side_token_factory'; // id required to prevent reexecution
+module.exports.tags = ['SideNFTTokenFactory', 'nft', '1.0.0'];
