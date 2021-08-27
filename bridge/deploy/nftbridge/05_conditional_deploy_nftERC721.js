fe62e51fff01be52e329d8b7c3adec0b426e70e5
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-27 11:49:49 -0300

    Add deployment for Federation with NFT

---
+++ bridge/deploy/nftbridge/05_conditional_deploy_nftERC721.js
@@ -0,0 +1,18 @@
+module.exports = async function ({getNamedAccounts, deployments, network}) { // HardhatRuntimeEnvironment
+  if (!network.live) {
+    const {deployer} = await getNamedAccounts()
+    const {deploy, log} = deployments
+
+    const deployResult = await deploy('NFTERC721TestToken', {
+      from: deployer,
+      args: ['The Drops', 'drop'],
+      log: true,
+    });
+
+    if (deployResult.newlyDeployed) {
+      log(`Contract NFTERC721TestToken deployed at ${deployResult.address}`);
+    }
+  }
+};
+module.exports.id = 'deploy_nft_erc721_test_token'; // id required to prevent reexecution
+module.exports.tags = ['ERC721', 'nft', '1.0.0'];
