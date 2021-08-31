2c0ce0181674807d416bed9def08b81048dcbf32
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-31 16:28:08 -0300

    fix tests and lint for bridge

--- bridge/contracts/SideTokenFactory/SideTokenFactoryV1.sol
+++ bridge/contracts/SideTokenFactory/SideTokenFactoryV1.sol
@@ -6,11 +6,11 @@ import "../zeppelin/ownership/Secondary.sol";
 import "../SideToken/SideTokenV1.sol";
 
 contract SideTokenFactoryV1 is Secondary {
-    event createdSideToken(address sideToken, string symbol);
+    event CreatedSideToken(address sideToken, string symbol);
 
     function createSideToken(string calldata name, string calldata symbol) external onlyPrimary returns(SideTokenV1) {
         SideTokenV1 sideToken = new SideTokenV1(name, symbol, primary());
-        emit createdSideToken(address(sideToken), symbol);
+        emit CreatedSideToken(address(sideToken), symbol);
         return sideToken;
     }
 }
