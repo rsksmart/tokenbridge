599f3ed3c27d416cb67db72d9ee6edc79f59f9e6
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-08-31 13:47:19 -0300

    SideToken contract versions

--- bridge/contracts/SideTokenFactory/SideTokenFactoryV1.sol
+++ bridge/contracts/SideTokenFactory/SideTokenFactoryV1.sol
@@ -3,13 +3,13 @@
 pragma solidity ^0.7.0;
 
 import "../zeppelin/ownership/Secondary.sol";
-import "../previous/SideToken_old.sol";
+import "../SideToken/SideTokenV1.sol";
 
 contract SideTokenFactoryV1 is Secondary {
     event createdSideToken(address sideToken, string symbol);
 
-    function createSideToken(string calldata name, string calldata symbol) external onlyPrimary returns(SideToken_old) {
-        SideToken_old sideToken = new SideToken_old(name, symbol, primary());
+    function createSideToken(string calldata name, string calldata symbol) external onlyPrimary returns(SideTokenV1) {
+        SideTokenV1 sideToken = new SideTokenV1(name, symbol, primary());
         emit createdSideToken(address(sideToken), symbol);
         return sideToken;
     }
