d138b7cd956d16378fbbfb432bbe414b239afbc5
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-09-02 18:02:05 -0300

    add old contracts to skip coverage

--- bridge/.solcover.js
+++ bridge/.solcover.js
@@ -1,7 +1,14 @@
 module.exports = {
-    skipFiles: ['test','zeppelin', 'previous'],
-    providerOptions: {
-      network_id: 5888,
-      //vmErrorsOnRPCResponse: false
-    }
-  };
+  skipFiles: [
+    'test',
+    'zeppelin',
+    'previous',
+    'contracts/Federation/FederationV1.sol',
+    'contracts/SideToken/SideTokenFactoryV1.sol',
+    'contracts/SideToken/SideTokenV1.sol'
+  ],
+  providerOptions: {
+    network_id: 5888,
+    //vmErrorsOnRPCResponse: false
+  }
+};
