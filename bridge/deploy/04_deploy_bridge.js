51cbf4eac53bb532e653ef290a02339b28f36e1d
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-09-03 10:01:10 -0300

    remove coments and ignored deployments from dev tests

--- bridge/deploy/04_deploy_bridge.js
+++ bridge/deploy/04_deploy_bridge.js
@@ -8,22 +8,10 @@ module.exports = async function ({getNamedAccounts, deployments}) { // HardhatRu
     from: deployer,
     log: true,
   });
+
   if (deployResult.newlyDeployed) {
     log(`Contract Bridge deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed.toString()} gas`);
   }
-
-  // We need the multisig to update the contract
-  // const MultiSigWallet = await deployments.get('MultiSigWallet');
-  // const Bridge = await deployments.get('Bridge');
 };
 module.exports.id = 'deploy_bridge'; // id required to prevent reexecution
 module.exports.tags = ['Bridge', 'new'];
-// module.exports.dependencies = [
-//     'BridgeProxy',
-//     'MultiSigWallet',
-//     'ProxyAdmin',
-//     'Federation',
-//     'AllowTokens',
-//     'SideTokenFactory',
-//     'WrappedCurrency'
-// ];
