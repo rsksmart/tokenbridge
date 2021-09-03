1bb4f13aabed17d0d13caa063416991c50c83f54
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-09-03 10:04:19 -0300

    add some files to code coverage skip files

--- bridge/.solcover.js
+++ bridge/.solcover.js
@@ -2,9 +2,11 @@ module.exports = {
   skipFiles: [
     'test',
     'zeppelin',
+    'Utils/UtilsV1.sol',
+    'Bridge/BridgeV2.sol',
+    'SideToken/SideTokenV1.sol',
     'Federation/FederationV1.sol',
     'SideTokenFactory/SideTokenFactoryV1.sol',
-    'SideToken/SideTokenV1.sol'
   ],
   providerOptions: {
     network_id: 5888,
