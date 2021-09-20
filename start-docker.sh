8fa061906ec79f6073661532ca7cd6d7dbaf849f
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-09-20 09:20:56 -0300

    add check for volume files

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,17 @@
-docker build -t tokenbridge:latest .
+#!/bin/sh
+
+docker build --no-cache -t tokenbridge:latest .
+
+FEDERATOR=$PWD/federator
+FEDERATOR_DB=$FEDERATOR/db
+FEDERATOR_LOG=$FEDERATOR/federator.log
+FEDERATOR_HEART=$FEDERATOR/heartbeat.log
+
+mkdir -p $FEDERATOR
+test -f $FEDERATOR_DB || touch $FEDERATOR_DB
+test -f $FEDERATOR_HEART || touch $FEDERATOR_HEART
+test -f $FEDERATOR_LOG || touch $FEDERATOR_LOG
+
 docker run --rm \
 	-v $PWD/federator/db:/app/federator/db \
 	-v $PWD/federator/heartbeat.log:/app/federator/heartbeat.log \
