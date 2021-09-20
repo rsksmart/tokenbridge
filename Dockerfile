dcdfac723e360562674569edaf95fee9bdedb4ad
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-09-20 12:13:58 -0300

    fix node user at dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM node:16.6.1-alpine3.13
+USER node
 
 RUN apk add --no-cache python2 build-base
 
 COPY ./federator /app/federator
 COPY ./bridge/abi /app/bridge/abi
 
-RUN chown -R node:node /app
-USER node
 WORKDIR /app/federator
 
 RUN npm ci
--- start-docker.sh
+++ start-docker.sh
@@ -2,19 +2,6 @@
 
 docker build --no-cache -t tokenbridge:latest .
 
-FEDERATOR=$PWD/federator
-FEDERATOR_DB=$FEDERATOR/db
-FEDERATOR_LOG=$FEDERATOR/federator.log
-FEDERATOR_HEART=$FEDERATOR/heartbeat.log
-
-mkdir -p $FEDERATOR
-test -f $FEDERATOR_DB || touch $FEDERATOR_DB
-test -f $FEDERATOR_HEART || touch $FEDERATOR_HEART
-test -f $FEDERATOR_LOG || touch $FEDERATOR_LOG
-
 docker run --rm \
-	-v $PWD/federator/db:/app/federator/db \
-	-v $PWD/federator/heartbeat.log:/app/federator/heartbeat.log \
 	-v $PWD/federator/config/config.js:/app/federator/built/federator/config/config.js \
-	-v $PWD/federator/federator.log:/app/federator/federator.log \
 	tokenbridge:latest
