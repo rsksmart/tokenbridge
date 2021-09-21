7e91dfd93e02630a38ff9af58552d5dda5389100
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-09-21 13:52:01 -0300

    changes for devops

--- start-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker build --no-cache -t tokenbridge:latest .
-
-docker run --rm \
-	-v $PWD/federator/config/config.js:/app/federator/built/federator/config/config.js \
-	tokenbridge:latest
