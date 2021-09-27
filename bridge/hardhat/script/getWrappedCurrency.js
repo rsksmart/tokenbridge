f47bc0792c3c12fd4a95a791834a8c12d1a734e8
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-09-27 11:24:04 -0300

    updated scripts

--- bridge/hardhat/script/getWrappedCurrency.js
+++ bridge/hardhat/script/getWrappedCurrency.js
@@ -1,4 +1,4 @@
-// How to run the script: npx hardhat run ./hardhat/script/getWrappedCurrency.js --network kovan rsktestnet rsktestnetbsc
+// How to run the script: npx hardhat run ./hardhat/script/getWrappedCurrency.js --network kovan bsctestnet rsktestnet rsktestnetbsc
 const hre = require("hardhat");
 
 async function main() {
