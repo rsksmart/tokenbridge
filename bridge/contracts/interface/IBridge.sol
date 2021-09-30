f1206111b7b725c4e304e27e8c2c74ee9fcaeac5
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-09-30 18:08:08 -0300

    update contracts to multichain

--- bridge/contracts/interface/IBridge.sol
+++ bridge/contracts/interface/IBridge.sol
@@ -4,113 +4,141 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 interface IBridge {
 
-    struct ClaimData {
-        address payable to;
-        uint256 amount;
-        bytes32 blockHash;
-        bytes32 transactionHash;
-        uint32 logIndex;
-    }
+	struct ClaimData {
+		address payable to;
+		uint256 amount;
+		bytes32 blockHash;
+		bytes32 transactionHash;
+		uint32 logIndex;
+	}
 
-    function version() external pure returns (string memory);
+	function version() external pure returns (string memory);
 
-    function getFeePercentage() external view returns(uint);
+	function getFeePercentage() external view returns(uint);
 
-    /**
-     * ERC-20 tokens approve and transferFrom pattern
-     * See https://eips.ethereum.org/EIPS/eip-20#transferfrom
-     */
-    function receiveTokensTo(address tokenToUse, address to, uint256 amount) external;
+	/**
+		* ERC-20 tokens approve and transferFrom pattern
+		* See https://eips.ethereum.org/EIPS/eip-20#transferfrom
+		*/
+	function receiveTokensTo(address tokenToUse, address to, uint256 amount) external;
 
-    /**
-     * Use network currency and cross it.
-     */
-    function depositTo(address to) external payable;
+	/**
+		* Use network currency and cross it.
+		*/
+	function depositTo(address to) external payable;
 
-    /**
-     * ERC-777 tokensReceived hook allows to send tokens to a contract and notify it in a single transaction
-     * See https://eips.ethereum.org/EIPS/eip-777#motivation for details
-     */
-    function tokensReceived (
-        address operator,
-        address from,
-        address to,
-        uint amount,
-        bytes calldata userData,
-        bytes calldata operatorData
-    ) external;
+	/**
+		* ERC-777 tokensReceived hook allows to send tokens to a contract and notify it in a single transaction
+		* See https://eips.ethereum.org/EIPS/eip-777#motivation for details
+		*/
+	function tokensReceived (
+		address operator,
+		address from,
+		address to,
+		uint amount,
+		bytes calldata userData,
+		bytes calldata operatorData
+	) external;
 
-    /**
-     * Accepts the transaction from the other chain that was voted and sent by the Federation contract
-     */
-    function acceptTransfer(
-        address _originalTokenAddress,
-        address payable _from,
-        address payable _to,
-        uint256 _amount,
-        bytes32 _blockHash,
-        bytes32 _transactionHash,
-        uint32 _logIndex
-    ) external;
+	/**
+		* ERC-777 tokensReceived hook allows to send tokens to a contract and notify it in a single transaction
+		* See https://eips.ethereum.org/EIPS/eip-777#motivation for details
+		*/
+	function tokensReceived (
+		address operator,
+		address from,
+		address to,
+		uint amount,
+		bytes calldata userData,
+		uint256 chainId
+	) external;
 
-    /**
-     * Claims the crossed transaction using the hash, this sends the funds to the address indicated in
-     */
-    function claim(ClaimData calldata _claimData) external returns (uint256 receivedAmount);
+	/**
+		* Accepts the transaction from the other chain that was voted and sent by the Federation contract
+		*/
+	function acceptTransfer(
+		address _originalTokenAddress,
+		address payable _from,
+		address payable _to,
+		uint256 _amount,
+		bytes32 _blockHash,
+		bytes32 _transactionHash,
+		uint32 _logIndex,
+		uint256 chainId
+	) external;
 
-    function claimFallback(ClaimData calldata _claimData) external returns (uint256 receivedAmount);
+	/**
+		* Claims the crossed transaction using the hash, this sends the funds to the address indicated in
+		*/
+	function claim(ClaimData calldata _claimData) external returns (uint256 receivedAmount);
 
-    function claimGasless(
-        ClaimData calldata _claimData,
-        address payable _relayer,
-        uint256 _fee,
-        uint256 _deadline,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    ) external returns (uint256 receivedAmount);
+	function claimFallback(ClaimData calldata _claimData) external returns (uint256 receivedAmount);
 
-    function getTransactionDataHash(
-        address _to,
-        uint256 _amount,
-        bytes32 _blockHash,
-        bytes32 _transactionHash,
-        uint32 _logIndex
-    ) external returns(bytes32);
+	function claimGasless(
+		ClaimData calldata _claimData,
+		address payable _relayer,
+		uint256 _fee,
+		uint256 _deadline,
+		uint8 _v,
+		bytes32 _r,
+		bytes32 _s
+	) external returns (uint256 receivedAmount);
 
-    event Cross(
-        address indexed _tokenAddress,
-        address indexed _from,
-        address indexed _to,
-        uint256 _amount,
-        bytes _userData
-    );
-    event NewSideToken(
-        address indexed _newSideTokenAddress,
-        address indexed _originalTokenAddress,
-        string _newSymbol,
-        uint256 _granularity
-    );
-    event AcceptedCrossTransfer(
-        bytes32 indexed _transactionHash,
-        address indexed _originalTokenAddress,
-        address indexed _to,
-        address  _from,
-        uint256 _amount,
-        bytes32 _blockHash,
-        uint256 _logIndex
-    );
-    event FeePercentageChanged(uint256 _amount);
-    event Claimed(
-        bytes32 indexed _transactionHash,
-        address indexed _originalTokenAddress,
-        address indexed _to,
-        address _sender,
-        uint256 _amount,
-        bytes32 _blockHash,
-        uint256 _logIndex,
-        address _reciever,
-        address _relayer,
-        uint256 _fee
-    );
+	function createSideToken(
+		uint256 _typeId,
+		address _originalTokenAddress,
+		uint8 _originalTokenDecimals,
+		string calldata _originalTokenSymbol,
+		string calldata _originalTokenName,
+		uint256 chainId
+	) external;
+
+	function getTransactionDataHash(
+		address _to,
+		uint256 _amount,
+		bytes32 _blockHash,
+		bytes32 _transactionHash,
+		uint32 _logIndex,
+		uint256 _chainId
+	) external returns(bytes32);
+
+	event Cross(
+		address indexed _tokenAddress,
+		address indexed _from,
+		address indexed _to,
+		uint256 _amount,
+		bytes _userData,
+		uint256 chainId
+	);
+	event NewSideToken(
+		address indexed _newSideTokenAddress,
+		address indexed _originalTokenAddress,
+		string _newSymbol,
+		uint256 _granularity,
+		uint256 chainId
+	);
+	event AcceptedCrossTransfer(
+		bytes32 indexed _transactionHash,
+		address indexed _originalTokenAddress,
+		address indexed _to,
+		address  _from,
+		uint256 _amount,
+		bytes32 _blockHash,
+		uint256 _logIndex,
+		uint256 _chainId
+	);
+	event FeePercentageChanged(uint256 _amount);
+	event Claimed(
+		bytes32 indexed _transactionHash,
+		address indexed _originalTokenAddress,
+		address indexed _to,
+		address _sender,
+		uint256 _amount,
+		bytes32 _blockHash,
+		uint256 _logIndex,
+		address _reciever,
+		address _relayer,
+		uint256 _fee,
+		uint256 _chainId
+	);
 }
