fd84c6edf8b0f699a8c6bd36cb299bccb96b17ce
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-10-01 15:18:01 -0300

    fix some code smells

--- bridge/hardhat/script/createTokensERC20Rsk.js
+++ bridge/hardhat/script/createTokensERC20Rsk.js
@@ -90,25 +90,25 @@ async function main() {
     console.log("\nBridgeProxy", BridgeProxy.address);
     console.log("\nMultiSigWallet", MultiSigWallet.address);
 
-    // const methodCallCreateSideToken = bridge.methods.createSideToken(
-    //   token.typeId,
-    //   token.originalTokenAddress,
-    //   tokenDecimals,
-    //   token.symbol,
-    //   token.name
-    // );
-    // const result = await methodCallCreateSideToken.call({ from: MultiSigWallet.address});
-    // console.log("Method call result", result);
+    const methodCallCreateSideToken = bridge.methods.createSideToken(
+      token.typeId,
+      token.originalTokenAddress,
+      tokenDecimals,
+      token.symbol,
+      token.name
+    );
+    const result = await methodCallCreateSideToken.call({ from: MultiSigWallet.address});
+    console.log("Method call result", result);
 
-    // const receipt = await multiSigContract.methods.submitTransaction(
-    //   BridgeProxy.address,
-    //   transactionEtherValue,
-    //   methodCallCreateSideToken.encodeABI()
-    // ).send({
-    //   from: deployer,
-    //   gasLimit: 3000000
-    // });
-    // console.log("Transaction worked", receipt.transactionHash);
+    const receipt = await multiSigContract.methods.submitTransaction(
+      BridgeProxy.address,
+      transactionEtherValue,
+      methodCallCreateSideToken.encodeABI()
+    ).send({
+      from: deployer,
+      gasLimit: 3000000
+    });
+    console.log("Transaction worked", receipt.transactionHash);
 
     const tokenAddressMapped = await bridge.methods.mappedTokens(token.originalTokenAddress).call({from: MultiSigWallet.address});
     console.log("Token address Mapped for", token.name, ":", tokenAddressMapped);
