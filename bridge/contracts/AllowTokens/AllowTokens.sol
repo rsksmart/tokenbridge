c64f4c32853032851cfd30e101a248de40a17913
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-10-01 21:32:57 -0300

    add authorization for method to set token

--- bridge/contracts/AllowTokens/AllowTokens.sol
+++ bridge/contracts/AllowTokens/AllowTokens.sol
@@ -61,7 +61,8 @@ contract AllowTokens is Initializable, UpgradableOwnable, UpgradableSecondary, I
 		return tokenInfoByTokenAddressByChain[chainId][tokenAddress];
 	}
 
-	function setTokenInfoByTokenAddress(uint256 chainId, address tokenAddress, TokenInfo memory info) public onlyOwner {
+	function setTokenInfoByTokenAddress(uint256 chainId, address tokenAddress, TokenInfo memory info) public {
+		require(isOwner() || _msgSender() == primary(), "AllowTokens: unauthorized sender");
 		tokenInfoByTokenAddressByChain[chainId][tokenAddress] = info;
 	}
 
