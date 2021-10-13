a54af59c27e325335e9e12d20dce1284be32e714
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-10-13 12:23:15 -0300

    fix federator integration tests

--- bridge/deploy/nftbridge/04_deploy_federationERC721.js
+++ bridge/deploy/nftbridge/04_deploy_federationERC721.js
@@ -5,16 +5,16 @@ module.exports = async function (hre) { // HardhatRuntimeEnvironment
   const {deployer} = await getNamedAccounts();
   const {deploy, log} = deployments;
 
-  const deployResult = await deploy('Federation', {
+  const deployResult = await deploy('FederationV3', {
     from: deployer,
     log: true,
   });
 
   if (deployResult.newlyDeployed) {
-    log(`Contract Federation with set Bridge deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed.toString()} gas`);
+    log(`Contract FederationV3 with set NFT Bridge deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed.toString()} gas`);
   }
 
-  const federationDeployment = await deployments.get('Federation');
+  const federationV3Deployment = await deployments.get('FederationV3');
   const proxyAdminDeployment = await deployments.get('ProxyAdmin');
   const nftBridgeProxyDeployment = await deployments.get('NftBridgeProxy');
   const federationProxyDeployment = await deployments.get('FederationProxy');
@@ -22,7 +22,7 @@ module.exports = async function (hre) { // HardhatRuntimeEnvironment
   const multiSigAddress = await address.getMultiSigAddress(hre);
 
   const proxyAdminContract = new web3.eth.Contract(proxyAdminDeployment.abi, proxyAdminDeployment.address);
-  const methodCallUpdagradeFederationDeployment = proxyAdminContract.methods.upgrade(federationProxyDeployment.address, federationDeployment.address);
+  const methodCallUpdagradeFederationDeployment = proxyAdminContract.methods.upgrade(federationProxyDeployment.address, federationV3Deployment.address);
   await methodCallUpdagradeFederationDeployment.call({ from: multiSigAddress });
 
   const multiSigContract = new web3.eth.Contract(multiSigWalletDeployment.abi, multiSigAddress);
@@ -33,7 +33,7 @@ module.exports = async function (hre) { // HardhatRuntimeEnvironment
   ).send({ from: deployer });
   log(`MultiSig submitTransaction upgrade FederationProxy contract in ProxyAdmin`);
 
-  const federation = new web3.eth.Contract(federationDeployment.abi, federationProxyDeployment.address);
+  const federation = new web3.eth.Contract(federationV3Deployment.abi, federationProxyDeployment.address);
   const methodCallSetNftBridge = federation.methods.setNFTBridge(nftBridgeProxyDeployment.address);
   await methodCallSetNftBridge.call({ from: multiSigAddress });
   await multiSigContract.methods.submitTransaction(federationProxyDeployment.address, 0, methodCallSetNftBridge.encodeABI())
@@ -42,4 +42,4 @@ module.exports = async function (hre) { // HardhatRuntimeEnvironment
 };
 module.exports.id = 'deploy_nft_federation'; // id required to prevent reexecution
 module.exports.tags = ['FederationV3', 'nft', '3.0.0', 'IntegrationTestNft'];
-module.exports.dependencies = ['Federation', 'ProxyAdmin', 'NftBridgeProxy', 'FederationProxy', 'MultiSigWallet'];
+module.exports.dependencies = ['ProxyAdmin', 'NftBridgeProxy', 'FederationProxy', 'MultiSigWallet'];
