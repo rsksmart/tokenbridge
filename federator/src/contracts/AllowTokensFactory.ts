9125bffb43992b3d0605d328d87836761fa091ad
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-12-06 09:19:01 -0300

    add multichain run to federator

--- federator/src/contracts/AllowTokensFactory.ts
+++ federator/src/contracts/AllowTokensFactory.ts
@@ -5,25 +5,23 @@ import { IAllowTokensV1 } from './IAllowTokensV1';
 import { IAllowTokensV0 } from './IAllowTokensV0';
 import CustomError from '../lib/CustomError';
 import utils from '../lib/utils';
-import { Config } from '../lib/config';
 import { ContractFactory } from './ContractFactory';
 import { AbiItem } from 'web3-utils';
 import { VERSIONS } from './Constants';
 import { Logger } from 'log4js';
+import { ConfigChain } from '../lib/configChain';
+import { Config } from '../lib/config';
 
 export class AllowTokensFactory extends ContractFactory {
   mainChainBridgeContract: any;
   sideChainBridgeContract: any;
-  constructor(config: Config, logger: Logger) {
-    super(config, logger);
+  constructor(config: Config, logger: Logger, sideChain: ConfigChain) {
+    super(config, logger, sideChain);
     this.mainChainBridgeContract = new this.mainWeb3.eth.Contract(
       abiBridgeV3 as AbiItem[],
       this.config.mainchain.bridge,
     );
-    this.sideChainBridgeContract = new this.sideWeb3.eth.Contract(
-      abiBridgeV3 as AbiItem[],
-      this.config.sidechain.bridge,
-    );
+    this.sideChainBridgeContract = new this.sideWeb3.eth.Contract(abiBridgeV3 as AbiItem[], sideChain.bridge);
   }
 
   async getVersion(allowTokensContract) {
