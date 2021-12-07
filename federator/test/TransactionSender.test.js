e4fe062f6dbcffeea2add933180f1ad56ad4d8cc
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-12-07 18:29:03 -0300

    set hartbeat as default

--- federator/test/TransactionSender.test.js
+++ federator/test/TransactionSender.test.js
@@ -24,7 +24,7 @@ describe('TransactionSender module tests', () => {
     it('should getNonce', async () => {
         let expected = '213';
         web3Mock.eth.getTransactionCount = jest.fn().mockReturnValue(Promise.resolve(expected));
-        let sender = new TransactionSender(web3Mock, logger, {});
+        let sender = new TransactionSender.default(web3Mock, logger, {});
         let result = await sender.getNonce();
         expect(result).toEqual(expected);
     });
@@ -32,12 +32,12 @@ describe('TransactionSender module tests', () => {
     it('should getAddress From privateKey', async () => {
         const pk = '3f28f888373e9ad1651a1227a5efdc0d7ea55bce6de3b5448de56c8588c6bd4d';
         const expectedAddr = '0x3444f14CbC7081ADEd7203E32E65304D17fe3bdA';
-        let sender = new TransactionSender(web3Mock, logger, {});
+        let sender = new TransactionSender.default(web3Mock, logger, {});
         let result = await sender.getAddress(pk); //Rsk Testnet
         expect(result).toEqual(expectedAddr.toLocaleLowerCase());
 
         web3Mock.eth.getAccounts = jest.fn().mockReturnValue(Promise.resolve([expectedAddr.toLocaleLowerCase()]));
-        sender = new TransactionSender(web3Mock, logger, {});
+        sender = new TransactionSender.default(web3Mock, logger, {});
         result = await sender.getAddress('');
         expect(result).toEqual(expectedAddr.toLocaleLowerCase());
         result = await sender.getAddress(undefined);
