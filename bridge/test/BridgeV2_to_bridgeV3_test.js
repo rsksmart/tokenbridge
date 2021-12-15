3d265f0e2335773f884f6dce3b54c708f3815a2f
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-12-14 23:05:30 -0300

    removed trailling spaces

--- bridge/test/BridgeV2_to_bridgeV3_test.js
+++ bridge/test/BridgeV2_to_bridgeV3_test.js
@@ -47,7 +47,7 @@ contract('Bridge Multichain Deploy Check', async function (accounts) {
     this.federationV3 = await FederationV3.new();
     this.proxyAdmin = await ProxyAdmin.new();
     this.sideTokenFactory = await SideTokenFactory.new();
-    
+
     await this.allowTokens.initialize(allowTokensManager, allowTokensPrimary, 1, 1, 1, [
       { description: 'BTC', limits: {
           min:toWei('0.001'),
@@ -71,7 +71,13 @@ contract('Bridge Multichain Deploy Check', async function (accounts) {
 
   describe('Upgrate from BridgeV2 to BridgeV3', async function () {
     it('should update the contract version from v2 to v3', async function () {
-      const initializeDataBridgeV2 = await this.bridgeV2.contract.methods.initialize(bridgeManager, this.federationV2.address, this.allowTokensV0.address, this.sideTokenFactory.address, symbolPrefix).encodeABI();
+      const initializeDataBridgeV2 = await this.bridgeV2.contract.methods.initialize(
+        bridgeManager,
+        this.federationV2.address,
+        this.allowTokensV0.address,
+        this.sideTokenFactory.address,
+        symbolPrefix
+      ).encodeABI();
       const bridgeProxy = await BridgeProxy.new(this.bridgeV2.address, this.proxyAdmin.address, initializeDataBridgeV2);
       const bridgeV2Implementation = new web3.eth.Contract(this.bridgeV2.abi, bridgeProxy.address);
 
@@ -88,55 +94,55 @@ contract('Bridge Multichain Deploy Check', async function (accounts) {
     it('should ReceiveTokensTo start in v2 finish in v3', async function () {
       const initialBalance = await this.token.contract.methods.balanceOf(tokenOwner).call();
       const initializeDataBridgeV2 = await this.bridgeV2.contract.methods.initialize(
-        bridgeManager, 
-        this.federationV2.address, 
-        this.allowTokensV0.address, 
-        this.sideTokenFactory.address, 
+        bridgeManager,
+        this.federationV2.address,
+        this.allowTokensV0.address,
+        this.sideTokenFactory.address,
         symbolPrefix
       ).encodeABI();
 
       const initializeDataFederatorV2 = await this.federationV2.contract.methods.initialize(
-        [federatorMember1, federatorMember2], 
-        2, 
-        this.bridgeV3.address, 
+        [federatorMember1, federatorMember2],
+        2,
+        this.bridgeV3.address,
         federatorOwner
       ).encodeABI();
-      
+
       const bridgeProxy = await BridgeProxy.new(this.bridgeV2.address, this.proxyAdmin.address, initializeDataBridgeV2);
       const federationProxy = await FederationProxy.new(this.federationV2.address, this.proxyAdmin.address, initializeDataFederatorV2);
 
       const bridgeV2Implementation = new web3.eth.Contract(this.bridgeV2.abi, bridgeProxy.address);
       const federationV2Implementation = new web3.eth.Contract(this.federationV2.abi, federationProxy.address);
-      
+
       await bridgeV2Implementation.methods.changeFederation(federationProxy.address).send({from: bridgeManager});
       await federationV2Implementation.methods.setBridge(bridgeProxy.address).send({from: federatorOwner});
 
       const amount = toWei('1000');
-      
+
       const tokenAllowed = await this.allowTokensV0.isTokenAllowed(this.token.address);
       assert.equal(tokenAllowed, true);
 
       let receipt = await this.token.approve(bridgeProxy.address, amount, { from: tokenOwner });
       utils.checkRcpt(receipt);
       receipt = await bridgeV2Implementation.methods.receiveTokens(
-        this.token.address, 
+        this.token.address,
         amount
       ).send({from: tokenOwner});
       const crossEvent = receipt.events.Cross;
-      
+
       const balanceAfterReceiveTokens = await this.token.contract.methods.balanceOf(tokenOwner).call();
       assert.equal(new web3.utils.BN(balanceAfterReceiveTokens).add(new web3.utils.BN(amount)).toString(), initialBalance);
 
       await federationV2Implementation.methods.voteTransaction(
-        this.token.address, 
-        tokenOwner, 
-        tokenOwner, 
-        amount, 
-        crossEvent.blockHash, 
-        crossEvent.transactionHash, 
+        this.token.address,
+        tokenOwner,
+        tokenOwner,
+        amount,
+        crossEvent.blockHash,
+        crossEvent.transactionHash,
         crossEvent.logIndex,
-      ).send({from: federatorMember1});      
-      
+      ).send({from: federatorMember1});
+
       await this.proxyAdmin.upgrade(bridgeProxy.address, this.bridgeV3.address);
       await this.proxyAdmin.upgrade(federationProxy.address, this.federationV3.address);
 
@@ -144,29 +150,29 @@ contract('Bridge Multichain Deploy Check', async function (accounts) {
       const federationV3Implementation = new web3.eth.Contract(this.federationV3.abi, federationProxy.address);
 
       await federationV3Implementation.methods.voteTransaction(
-        this.token.address, 
-        tokenOwner, 
-        tokenOwner, 
-        amount, 
-        crossEvent.blockHash, 
-        crossEvent.transactionHash, 
+        this.token.address,
+        tokenOwner,
+        tokenOwner,
+        amount,
+        crossEvent.blockHash,
+        crossEvent.transactionHash,
         crossEvent.logIndex,
         utils.tokenType.COIN,
         chains.ETHEREUM_MAIN_NET_CHAIN_ID,
         chains.HARDHAT_TEST_NET_CHAIN_ID,
-      ).send({from: federatorMember2});      
-      
+      ).send({from: federatorMember2});
+
       await bridgeV3Implementation.methods.claim(
         {
           to: tokenOwner,
           amount: amount,
-          blockHash: crossEvent.blockHash, 
+          blockHash: crossEvent.blockHash,
           transactionHash: crossEvent.transactionHash,
           logIndex: crossEvent.logIndex,
           originChainId: chains.ETHEREUM_MAIN_NET_CHAIN_ID,
         },
       ).send({from: tokenOwner});
-      
+
       const originalTokenAddress = await bridgeV3Implementation.methods.originalTokenAddresses(crossEvent.transactionHash).call();
       const originalTokenContract = new web3.eth.Contract(ERC777.abi, originalTokenAddress );
 
