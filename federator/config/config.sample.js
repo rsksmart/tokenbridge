1235975621bd08a92cdd8b7d6654fa601c67b32f
pmprete <pedro@thinkanddev.com>
2022-01-17 11:04:34 -0300

    WIP Heartbeat now events an event with an array of information

--- federator/config/config.sample.js
+++ federator/config/config.sample.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 module.exports = {
-    mainchain: require('./rsktestnet-kovan.json'), //the json containing the smart contract addresses in rsk
-    sidechain: require('./kovan.json'), //the json containing the smart contract addresses in eth
-    runEvery: 2, // In minutes,
-    confirmations: 120, // Number of blocks before processing it, if working with ganache set as 0
-    privateKey: fs.readFileSync(`${__dirname}/federator.key`, 'utf8'),
-    storagePath: './db',
-    etherscanApiKey: '',
-    runHeartbeatEvery: 1, // In hours
-    endpointsPort: 5000, // Server port
-    useNft: false,
-}
+  mainchain: require("./rsktestnet-kovan.json"), //the json containing the smart contract addresses in rsk
+  sidechain: [
+    require("./kovan.json"), //the json containing the smart contract addresses in eth
+    require("./bsctestnet.json"), //the json containing the smart contract addresses in binance
+  ],
+  runEvery: 2, // In minutes,
+  confirmations: 120, // Number of blocks before processing it, if working with ganache set as 0
+  privateKey: fs.readFileSync(`${__dirname}/federator.key`, "utf8"),
+  storagePath: "./db",
+  etherscanApiKey: "",
+  runHeartbeatEvery: 1, // In hours
+  endpointsPort: 5000, // Server port
+  useNft: false,
+};
