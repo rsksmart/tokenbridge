39f7df53febbd55e7a96774210c2c89a4b3d59be
pmprete <pedro@thinkanddev.com>
2022-01-21 02:20:03 -0300

    Add upgrade deploy scripts, rename federatorProxy to federationProxy

--- bridge/deploy/upgrades/01_upgrade_bridge.js
+++ bridge/deploy/upgrades/01_upgrade_bridge.js
@@ -2,27 +2,25 @@ const address = require('../../hardhat/helper/address');
 
 module.exports = async function (hre) { // HardhatRuntimeEnvironment
   const {getNamedAccounts, deployments} = hre;
-  const {deployer} = await getNamedAccounts();
+  const {deployer, bridgeProxy, proxyAdmin, multiSig} = await getNamedAccounts();
   const {log} = deployments;
 
   const bridgeDeployment = await deployments.get('Bridge');
-  const proxyAdminDeployment = await deployments.get('ProxyAdmin');
-  const bridgeProxyDeployment = await deployments.get('BridgeProxy');
-  const multiSigAddress = await address.getMultiSigAddress(hre);
+  const proxyAdminArtifact = await deployments.get('ProxyAdmin');
   const multiSigWalletDeployment = await deployments.get('MultiSigWallet');
 
-  const proxyAdminContract = new web3.eth.Contract(proxyAdminDeployment.abi, proxyAdminDeployment.address);
-  const methodCallUpdagradeBridgeDeployment = proxyAdminContract.methods.upgrade(bridgeProxyDeployment.address, bridgeDeployment.address);
-  await methodCallUpdagradeBridgeDeployment.call({ from: multiSigAddress });
+  const proxyAdminContract = new web3.eth.Contract(proxyAdminArtifact.abi, proxyAdmin);
+  const methodCallUpdagradeBridgeDeployment = proxyAdminContract.methods.upgrade(bridgeProxy, bridgeDeployment.address);
+  await methodCallUpdagradeBridgeDeployment.call({ from: multiSig });
 
-  const multiSigContract = new web3.eth.Contract(multiSigWalletDeployment.abi, multiSigAddress);
+  const multiSigContract = new web3.eth.Contract(multiSigWalletDeployment.abi, multiSig);
   await multiSigContract.methods.submitTransaction(
-    proxyAdminDeployment.address,
+    proxyAdmin,
     0,
     methodCallUpdagradeBridgeDeployment.encodeABI(),
   ).send({ from: deployer });
   log(`MultiSig submitTransaction upgrade BridgeProxy contract in ProxyAdmin`);
 };
-module.exports.id = 'deploy_new_bridge'; // id required to prevent reexecution
+module.exports.id = 'upgrade_bridge'; // id required to prevent reexecution
 module.exports.tags = ['UpgradeBridge', 'Upgrade'];
 module.exports.dependencies = ['Bridge'];
