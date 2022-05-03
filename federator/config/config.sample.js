2be7bb748813957871839d5bf861d3c3500bd1fb
João Santos <joao.santos@iovlabs.org>
2022-05-03 16:08:29 -0300

    Remove all references of BINANCE.

--- federator/config/config.sample.js
+++ federator/config/config.sample.js
@@ -3,7 +3,6 @@ module.exports = {
   mainchain: require("./rsktestnet.json"), //the json containing the smart contract addresses in rsk
   sidechain: [
     require("./kovan.json"), //the json containing the smart contract addresses in eth
-    require("./bsctestnet.json"), //the json containing the smart contract addresses in binance
   ],
   runEvery: 2, // In minutes,
   privateKey: fs.readFileSync(`${__dirname}/federator.key`, "utf8"),
@@ -12,5 +11,5 @@ module.exports = {
   runHeartbeatEvery: 1, // In hours
   endpointsPort: 5000, // Server port
   useNft: false,
-  checkHttps: false
+  checkHttps: false,
 };
