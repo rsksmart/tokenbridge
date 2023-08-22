c23dcf8123857e301ca7582c66f5b5b190bd765b
Guilherme Soares <guilherme.soares@iovlabs.org>
2023-08-22 14:24:49 +0300

    Federator was extracted to another repository

--- federator/src/contracts/Constants.ts
+++
@@ -1,7 +0,0 @@
-export const VERSIONS = {
-  V4: 'v4',
-  V3: 'v3',
-  V2: 'v2',
-  V1: 'v1',
-  V0: 'v0',
-};
--- federator/src/lib/CustomError.ts
+++
@@ -1,6 +0,0 @@
-export class CustomError extends Error {
-  constructor(message: string, err: Error) {
-    super(message);
-    this.stack += '\n Internal ' + err.stack;
-  }
-}
